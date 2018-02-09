d95a69b6c8391a772fdfbf54fd132607ea63c0e1
mfasel <markus.fasel@cern.ch>
2018-02-09 17:55:52 +0100

    Adding macros plotting the constituent QA

---
+++ helpers/graphics.C
@@ -0,0 +1,32 @@
+#ifndef __CLING__
+#include <TH1.h>
+#endif
+
+struct Style {
+  Color_t color;
+  Style_t marker;
+
+  template<typename t>
+  void SetStyle(t &object) {
+    object.SetMarkerColor(color);
+    object.SetMarkerStyle(marker);
+    object.SetLineColor(color);  
+  }
+};
+
+TH1 *make_frame(const char *name, const char *xtitle, const char *ytitle, double *range) {
+  auto frame = new TH1F(name, "", 100, range[0], range[1]);
+  frame->SetDirectory(nullptr);
+  frame->SetStats(false);
+  frame->SetXTitle(xtitle);
+  frame->SetYTitle(ytitle);
+  frame->GetYaxis()->SetRangeUser(range[2], range[3]);
+  return frame;
+}
+
+template<typename t>
+void InitWidget(t &widget) {
+  widget.SetBorderSize(0);
+  widget.SetFillStyle(0);
+  widget.SetTextFont(42);
+}
