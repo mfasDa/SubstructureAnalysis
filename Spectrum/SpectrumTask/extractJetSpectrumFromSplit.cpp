aa4fd7565743b83213fd86358b7ecae3b8fbf9bb
mfasel <markus.fasel@cern.ch>
2018-05-28 19:18:06 +0200

    More jet spectra period comparison

---
+++ Spectrum/SpectrumTask/extractJetSpectrumFromSplit.cpp
@@ -0,0 +1,122 @@
+#ifndef __CLING__
+#include <array>
+#include <iomanip>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <ROOT/TSeq.hxx>
+#include <RStringView.h>
+#include <TFile.h>
+#include <THnSparse.h>
+#include <TH1.h>
+#include <TKey.h>
+#include <TList.h>
+#include <TString.h>
+#include <TSystem.h>
+#endif
+
+#include "../../helpers/filesystem.C"
+#include "../../helpers/string.C"
+
+TH1 *makeJetSparseProjection(THnSparse *hsparse, int triggercluster, bool nefcut) {
+  auto clusterbin = hsparse->GetAxis(4)->FindBin(triggercluster);
+  hsparse->GetAxis(4)->SetRange(clusterbin);
+  if(nefcut){
+    hsparse->GetAxis(3)->SetRangeUser(0., 0.98);
+  }
+
+  auto projected = hsparse->Projection(0);
+  projected->SetDirectory(nullptr);
+
+  // Unzoom
+  hsparse->GetAxis(4)->UnZoom();
+  if(nefcut) hsparse->GetAxis(3)->UnZoom();
+  return projected;
+}
+
+std::string getClusterName(int triggercluster) {
+  std::string clustername;
+  switch(triggercluster){
+    case 0: clustername = "ANY"; break;
+    case 1: clustername = "CENT"; break;
+    case 2: clustername = "CENTNOTRD"; break;
+    case 3: clustername = "CALO"; break;
+    case 4: clustername = "CALOFAST"; break;
+    case 5: clustername = "CENTBOTH"; break;
+    case 6: clustername = "ONLYCENT"; break;
+    case 7: clustername = "ONLYCENTNOTRD"; break;
+    case 8: clustername = "CALOBOTH"; break;
+    case 9: clustername = "ONLYCALO"; break;
+    case 10: clustername = "ONLYCALOFAST"; break;
+  };
+  return clustername;
+}
+
+TH1 *getNormalizedJetSpectrum(const std::string_view fname, double radius, const std::string_view jettype, const std::string_view trigger, int triggercluster, bool doScale){
+  std::unique_ptr<TFile> reader(TFile::Open(fname.data(), "READ"));
+  std::string dirname = reader->GetListOfKeys()->At(0)->GetName();
+  std::stringstream normalizedname;
+  reader->cd(dirname.data());
+  auto histlist = static_cast<TList *>(static_cast<TKey *>(gDirectory->GetListOfKeys()->At(0))->ReadObj());
+  auto jetsparse = static_cast<THnSparse *>(histlist->FindObject("hJetTHnSparse"));
+
+  auto projected = makeJetSparseProjection(jetsparse, triggercluster, jettype == std::string_view("FullJets"));
+  normalizedname << "JetSpectrum_" << jettype << "_R" << std::setw(2) << std::setfill('0') << int(radius * 10.) << "_" << trigger << "_" << getClusterName(triggercluster);
+  projected->SetName(normalizedname.str().data());
+  projected->SetDirectory(nullptr);
+  if(doScale){
+    auto norm = static_cast<TH1 *>(histlist->FindObject("hClusterCounter"));
+    auto clusterbin = norm->GetXaxis()->FindBin(triggercluster);
+    projected->Scale(1./norm->GetBinContent(clusterbin));
+  }
+  return projected;
+}
+
+std::string matchSpectrum(const std::vector<std::string> &files, const std::string_view jettype, double radius, const std::string_view trigger){
+  std::string result;
+  auto data = std::find_if(files.begin(), files.end(), [jettype, radius, trigger](const std::string &fname) -> bool {
+    TString fstr(fname);
+    return fstr.Contains(jettype.data()) && fstr.Contains(trigger.data()) && fstr.Contains(Form("R%02d", int(radius * 10.))); 
+  });
+  if(data != files.end()) result = *data;
+  return result;
+}
+
+std::vector<std::string> getListOfFiles(const std::string_view inputdir){
+  std::string files = gSystem->GetFromPipe("ls -1 | grep JetSpectrum").Data();
+  return tokenize(files, '\n');
+}
+
+void extractJetSpectrumFromSplit(const std::string_view inputdir = ".", int triggercluster = 0, bool doScale = true){
+  const std::array<const std::string, 2> kJetTypes = {{"FullJets", "NeutralJets"}};
+  const std::array<const std::string, 5> kTriggers = {{"INT7", "EG1", "EG2", "EJ1", "EJ2"}};
+
+  auto specfiles = getListOfFiles(inputdir);
+  
+  std::stringstream outputfile;
+  if(inputdir != std::string_view(".")) outputfile << inputdir << "/";
+  outputfile << "JetSpectra_" << getClusterName(triggercluster) << ".root";
+  std::unique_ptr<TFile> writer(TFile::Open(outputfile.str().data(), "RECREATE"));
+
+  for(const auto &jt : kJetTypes) {
+    for(const auto &trg : kTriggers) {
+      std::stringstream outdirname;
+      outdirname << jt << "_" << trg;
+      bool outputdirCreated(false);
+      for(auto rad : ROOT::TSeqI(2, 6)){
+        double radius = double(rad)/10.;
+        auto specfile = matchSpectrum(specfiles, jt, radius, trg);
+        if(!specfile.length()) continue;
+        std::cout << "Found spectra file " << specfile << std::endl;
+        if(!outputdirCreated) 
+        {
+          writer->mkdir(outdirname.str().data());
+          outputdirCreated = true;
+        }
+        auto spec = getNormalizedJetSpectrum(specfile, radius, jt, trg, triggercluster, doScale);
+        writer->cd(outdirname.str().data());
+        spec->Write();
+      }
+    }
+  }
+}
---
+++ Spectrum/SpectrumTask/makePeriodComparison1617.cpp
@@ -0,0 +1,94 @@
+#ifndef __CLING__
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+#include <ROOT/TSeq.hxx>
+#include <RStringView.h>
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TH1.h>
+#include <TKey.h>
+#include <TLegend.h>
+#include <TPaveText.h>
+
+#include "TAxisFrame.h"
+#include "TDefaultLegend.h"
+#include "TNDCLabel.h"
+#include "TSavableCanvas.h"
+#endif
+
+#include "../../helpers/graphics.C"
+
+std::string findDirectory(const TFile &reader, const std::string_view jettype, const std::string_view trigger){
+  std::string result;
+  for(auto d : *(gDirectory->GetListOfKeys())){
+    std::string_view keyname = d->GetName();
+    if(keyname.find(jettype) == std::string::npos) continue;
+    if(keyname.find(trigger) == std::string::npos) continue;
+    result = std::string(keyname); 
+  }
+  return result;
+}
+
+std::vector<TH1 *> readTriggerHistos(const std::string_view filename, const std::string_view jettype, const std::string_view trigger){
+  std::vector<TH1 *> result;
+  std::unique_ptr<TFile> reader(TFile::Open(filename.data(), "READ"));
+  auto dirname = findDirectory(*reader, jettype, trigger);
+  if(!dirname.length()) return result;
+  reader->cd(dirname.data());
+  for(auto k : *(gDirectory->GetListOfKeys())){
+    auto h = static_cast<TH1 *>(static_cast<TKey *>(k)->ReadObj()); 
+    h->SetDirectory(nullptr);
+    result.emplace_back(h);
+  }
+  return result;
+}
+
+void makePeriodComparison1617(const std::string_view jettype, const std::string_view trigger, const std::string_view triggercluster){
+  std::map<std::string, Style> periods = {
+    {"LHC16i", {kBlue, 25}}, {"LHC16j", {kGreen, 26}}, {"LHC16k", {kViolet, 27}}, {"LHC17", {kRed, 28}}
+  };
+
+  auto plot = new ROOT6tools::TSavableCanvas(Form("periodComparison%s%s", jettype.data(), trigger.data()), Form("Period comparison %s in trigger %s", jettype.data(), trigger.data()), 1200, 1000);
+  plot->Divide(2,2);
+
+  int ipad = 1;
+  TLegend *leg(nullptr);
+  for(auto irad : ROOT::TSeqI(2,6)){
+    plot->cd(ipad);
+    gPad->SetLogy();
+    (new ROOT6tools::TAxisFrame(Form("JetSpecFrameR%02d%s%s", irad, jettype.data(), trigger.data()), "p_{t,jet} (GeV/c)", "1/N_{trg} dN_{jet}/dp_{t,jet} ((GeV/c)^{-1}", 0., 200., 1e-7, 200))->Draw("axis");
+
+    if(ipad == 1) {
+      leg = new ROOT6tools::TDefaultLegend(0.65, 0.45, 0.89, 0.89);
+      leg->Draw();
+
+      (new ROOT6tools::TNDCLabel(0.15, 0.8, 0.5, 0.88, Form("%s, trigger %s", jettype.data(), trigger.data())))->Draw();
+    }
+
+    (new ROOT6tools::TNDCLabel(0.15, 0.72, 0.25, 0.79, Form("R=%.1f", double(irad)/10.)))->Draw();
+    ipad++;
+  }
+
+  for(const auto &p : periods){
+    std::stringstream infilename;
+    infilename << p.first << "/JetSpectra_" << triggercluster << ".root";
+    auto histos = readTriggerHistos(infilename.str(), jettype, trigger);
+    
+    ipad = 1;
+    for(auto irad : ROOT::TSeqI(2, 6)){
+      plot->cd(ipad);
+      auto hist = *std::find_if(histos.begin(), histos.end(), [irad](const TH1 *hist) -> bool { return std::string(hist->GetName()).find(Form("R%02d", irad)) != std::string::npos; });
+      p.second.SetStyle<TH1>(*hist);
+      hist->Draw("epsame");
+      if(ipad == 1) leg->AddEntry(hist, p.first.data(), "lep");
+      ipad++;
+    }
+  }
+
+  plot->cd();
+  plot->Update();
+  plot->SaveCanvas(plot->GetName());
+}
