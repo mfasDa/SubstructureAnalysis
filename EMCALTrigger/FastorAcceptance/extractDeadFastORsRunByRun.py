b093afe03e00f71538157167e97ce51b404894b8
mfasel <markus.fasel@cern.ch>
2018-06-04 11:19:49 +0200

    Make trigger type optional

--- EMCALTrigger/FastorAcceptance/extractDeadFastORsRunByRun.py
+++ EMCALTrigger/FastorAcceptance/extractDeadFastORsRunByRun.py
@@ -10,7 +10,7 @@ rootmacro = os.path.join(scriptdir, "extractDeadFastORsFreq.cpp")
 def get_runs(basedir):
   return sorted([r for r in os.listdir(basedir) if r.isdigit()])
 
-def extract_fastors(basedir):
+def extract_fastors(basedir, trigger):
   base = os.getcwd()
   for r in get_runs(basedir):
     rundir = os.path.join(base, r)
@@ -20,10 +20,10 @@ def extract_fastors(basedir):
     logging.info("Extracting dead FastORs or run %d ..." %int(r))
     os.chdir(rundir)
     for level in range(0, 2):
-      os.system("root -l -b -q \'%s(%d, \"EG1\")\'" %(rootmacro, level))
+      os.system("root -l -b -q \'%s(%d, \"%s\")\'" %(rootmacro, level, trigger))
     os.chdir(base)
 
 if __name__ == "__main__":
   LOGLEVEL = logging.INFO
   logging.basicConfig(format='[%(levelname)s]: %(message)s', level=LOGLEVEL)
-  extract_fastors(sys.argv[1] if len(sys.argv) > 1 else os.getcwd())
+  extract_fastors(basedir=sys.argv[2] if len(sys.argv) > 2 else os.getcwd(), trigger=sys.argv[1])
