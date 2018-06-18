025f46d3973da77ce2ffd62c182319a0e68adfb8
mfasel <markus.fasel@cern.ch>
2018-06-18 16:15:10 +0200

    Speedup copy process with threads

--- copyFromGrid.py
+++ copyFromGrid.py
@@ -1,78 +1,292 @@
 #! /usr/bin/env python
 
-import sys, os, commands, zipfile, logging, getopt, md5
+import commands
+import getopt
+import logging
+import md5
+import os
+import subprocess
+import sys
+import threading
+import zipfile
 
-def GetMD5(localfile):
-    m = md5()
-    with open(localfile, "rb") as f:
-        data = f.read() #read file in chunk and call update on each chunk if file is large.
-        m.update(data)
-    return m.hexdigest()
+class AlienTool: 
+    
+    def __init__(self):
+        self.__lock = threading.Lock()
+  
+    def md5(self, fname):
+        hash_md5 = hashlib.md5()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+               hash_md5.update(chunk)
+        return hash_md5.hexdigest()
 
-def aliencopy(sourcelocation, targetlocation):
-    logging.info("Copy %s to %s", sourcelocation, targetlocation)
-    if os.path.exists(targetlocation):
-        remotemd5 = int(commands.getstatusoutput("gbbox md5sum %s | cut -f1" %sourcelocation)[1], 16)
-        localmd5 = GetMD5(targetlocation)
-        if localmd5 == remotemd5:
-            logging.info("Not copying %s because it has been found uncorrupted locally" %sourcelocation)
-    os.system("alien_cp alien://%s %s" %(sourcelocation, targetlocation))
+    def gridmd5sum(self, gridfile):
+        # Function must be robust enough to fetch all possible
+        # xrd error states which it usually gets from the stdout
+        # of the query process
+        gbout = ''
+        errorstate = True
+        while errorstate:
+            errorstate = False
+            gbout = commands.getstatusoutput("gbbox md5sum %s" %gridfile)[1]
+            if gbout.startswith("Error") or gbout.startswith("Warning") or "CheckErrorStatus" in gbout:
+                errorstate = True
+        return gbout.split('\t')[0]
 
-def alienlist(inputdir):
-    logging.debug("Scanning directory: %s", inputdir)
-    content = []
-    tmplist = commands.getstatusoutput("alien_ls %s" %(inputdir))[1].split("\n")
-    for r in tmplist:
-        content.append(r.lstrip().rstrip())
-    return content
+    def checkconsistency(self, gridfile, localfile):
+        gmd5 = self.gridmd5sum(gridfile)
+        lmd5 = self.md5(localfile)
+        if lmd5 == gmd5:
+            return True
+        return False
 
-def extractZipfile(filename):
-    if not ".zip" in filename or not os.path.exists(filename):
-        return
-    cwd = os.getcwd()
-    os.chdir(os.path.dirname(filename))
-    #unzip
-    myarchive = zipfile.ZipFile(os.path.basename(filename))
-    myarchive.extractall()
-    os.chdir(cwd)
+    def copy(self, inputfile, outputfile):
+        logging.info("Copying %s to %s", inputfile, outputfile)
+        self.__lock.acquire(True)
+        if not os.path.exists(os.path.dirname(outputfile)):
+            os.makedirs(os.path.dirname(outputfile), 0755)
+        self.__lock.release()
+        subprocess.call(['alien_cp', 'alien://%s'%inputfile, outputfile])
+        # make check
+        if os.path.exists(outputfile):
+            localmd5 = self.md5(outputfile)
+            gridmd5 = self.gridmd5sum(inputfile)
+            if localmd5 != gridmd5:
+                logging.error("Mismatch in MD5 sum for file %s", outputfile)
+                # incorrect MD5sum, outputfile probably corrupted
+                os.remove(outputfile)
+                return False
+            else:
+                logging.info("Output file %s copied correctly", outputfile)
+                return True
+        else:
+            logging.error("output file %s not found" %outputfile)
+            return False
 
-def TransferFile(inputfile, outputfile):
-    if not os.path.exists(os.path.dirname(outputfile)):
-        os.makedirs(os.path.dirname(outputfile), 0755)
-    aliencopy(inputfile, outputfile)
-    extractZipfile(outputfile)
+    def listdir(self, inputdir):
+        # Function must be robust agianst error states which
+        # it can only get from the stdout. As long as the 
+        # request ends in error state it should retry
+        errorstate = True
+        while errorstate:
+            dirs = commands.getstatusoutput("alien_ls %s" %inputdir)[1]
+            errorstate = False
+            result = []
+            for d in dirs.split("\n"):
+                if d.startswith("Error") or d.startswith("Warning"):
+                    errorstate = True
+                    break
+                mydir = d.rstrip().lstrip()
+                if len(mydir):
+                    result.append(mydir)
+            if errorstate:
+                continue
+            return result
+
+class Filepair:
+  
+    def __init__(self, source, target, ntrials = 0):
+        self.__source = source
+        self.__target = target
+        self.__ntrials = ntrials
+
+    def setntrials(self, ntrials):
+        self.__ntrials = ntrials
+
+    def getntrials(self):
+        return self.__ntrials
+
+    def source(self):
+        return self.__source
+
+    def target(self):
+        return self.__target
+
+class DataPool :
+  
+    def __init__(self):
+        self.__data = []
+        self.__lock = threading.Lock()
+
+    def insert_pool(self, filepair):
+        self.__lock.acquire(True)
+        logging.info("Adding to pool: %s - %s", filepair.source(), filepair.target())
+        self.__data.append(filepair)
+        self.__lock.release()
+
+    def getpoolsize(self):
+        return len(self.__data)
+
+    def pop(self):
+        result = None
+        self.__lock.acquire(True)
+        if(self.getpoolsize()):
+            result = self.__data.pop(0)
+        self.__lock.release()
+        return result
+
+class CopyHandler(threading.Thread):
+  
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.__datapool = None
+        self.__poolfiller = None
+        self.__alienhelper = None
+        self.__maxtrials = 5
+  
+    def setalienhelper(self, alienhelper):
+        self.__alienhelper = alienhelper
+  
+    def setdatapool(self, datapool):
+        self.__datapool = datapool
+
+    def setpoolfiller(self, poolfiller):
+        self.__poolfiller = poolfiller
+
+    def setmaxtrials(self, maxtrials):
+        self.__maxtrials = maxtrials
+
+    def __extractZipfile(self, filename):
+        if not ".zip" in filename or not os.path.exists(filename):
+            return
+        cwd = os.getcwd()
+        os.chdir(os.path.dirname(filename))
+        #unzip
+        myarchive = zipfile.ZipFile(os.path.basename(filename))
+        myarchive.extractall()
+        os.chdir(cwd)
+
+    def __waitforwork(self):
+        if self.__datapool.getpoolsize():
+            return
+        if not self.__poolfiller.isactive():
+            return
+        while not self.__datapool.getpoolsize():
+            if not self.__poolfiller.isactive():
+            break
+        time.sleep(5)
+
+    def run(self):
+        hasWork = True
+        while hasWork:
+            self.__waitforwork()
+            nextfile = self.__datapool.pop()
+            if nextfile:
+                copystatus = self.__alienhelper.copy(nextfile.source(), nextfile.target())
+                if not copystatus:
+                    # put file back on the pool in case of copy failure
+                    # only allow for amaximum amount of copy trials
+                    trials = nextfile.getntrials()
+                    trials += 1
+                    if trials >= self.__maxtrials:
+                        logging.error("File %s failed copying in %d trials - giving up", nextfile.source(), self.__maxtrials)
+                    else:
+                        logging.error("File %s failed copying (%d/%d) - re-inserting into the pool ...", nextfile.source(), trials, self.__maxtrials)
+                        nextfile.setntrials(trials)
+                        self.__datapool.insert_pool(nextfile)
+                else:
+                    # Copy successfull - extract the zipfile (if it is a zipfile)
+                    self.__extractZipfile(nextfile.target())
+            if not self.__poolfiller.isactive():
+                # if pool is empty exit, else keep thread alive for remaining files
+                if not self.__datapool.getpoolsize():
+                hasWork = False
+
+class PoolFiller(threading.Thread):
+    
+    def __init__(self, sample, trainrun, outputlocation, targetfile, maxpoolsize):
+        threading.Thread.__init__(self)
+        self.__datapool = None
+        self.__alientool = None
+        self.__isactive = False
+        self.__sample = sample
+        self.__trainrun = trainrun
+        self.__outputlocation = outputlocation
+        self.__targetfile = targetfile
+        self.__maxpoolsize = maxpoolsize
+
+    def setalientool(self, alientool):
+        self.__alientool = alientool
+
+    def setdatapool(self, datapool):
+        self.__datapool = datapool
+
+    def isactive(self):
+        return self.__isactive
+    
+    def run():
+        self.__isactive = True
+        self.__find_files()
+        self.__isactive = False
+
+    def __wait(self):
+        # wait until pool is half empty
+        if self.__datapool.getpoolsize() < self.__maxpoolsize:
+            return
+        # pool full, wait until half empty
+        emptylimit = self.__maxpoolsize/2
+        while self.__datapool.getpoolsize() > emptylimit:
+            time.sleep(5)
+
+    def __find_files(self):
+        run = 0
+        ptharbin = 0
+        for levelone in self.__alientool.listdir(self.__sample):
+            # both run and pt-hard bin must be numeric
+            if not levelone.isdigit():
+                continue
+            isrun = False
+            if int(levelone) >= 190000:
+                isrun = True
+                run = int(levelone)
+            else:
+                isrun = False
+                pthardbin = int(levelone)
+            tmppath = os.path.join(self.__sample, levelone)
+            logging.info("Doing %s %s", "run" if isrun else "pthard-bin", levelone)
+            for leveltwo in self.__alientool.listdir(tmppath):
+                if not leveltwo.isdigit():
+                    continue
+                if isrun:
+                    pthardbin = int(leveltwo)
+                else:
+                    run = int(leveltwo)
+                logging.info("Doing %s %s", "pthard-bin" if isrun else "run", leveltwo)
+                tmppathtwo = os.path.join(tmppath, leveltwo)
+                inputdir= os.path.join(tmppathtwo, self.__trainrun)
+                inputfile = os.path.join(inputdir, self.__targetfile)
+                outputdir = os.path.join(os.path.abspath(self.__outputlocation), "%02d" %pthardbin, "%d" %run)
+                outputfile = os.path.join(outputdir, self.__targetfile)
+                if not os.path.exists(outputfile):
+                    self.__datapool.insert_pool(Filepair(inputfile, outputfile))
+                    self.__wait()
 
 def transfer(sample, trainrun, outputlocation, targetfile):
-    run = 0
-    ptharbin = 0
-    for levelone in alienlist(sample):
-        # both run and pt-hard bin must be numeric
-        if not levelone.isdigit():
-            continue
-        isrun = False
-        if int(levelone) >= 190000:
-            isrun = True
-            run = int(levelone)
-        else:
-            isrun = False
-            pthardbin = int(levelone)
-        tmppath = os.path.join(sample, levelone)
-        logging.info("Doing %s %s", "run" if isrun else "pthard-bin", levelone)
-        for leveltwo in alienlist(tmppath):
-            if not leveltwo.isdigit():
-                continue
-            if isrun:
-                pthardbin = int(leveltwo)
-            else:
-                run = int(leveltwo)
-            logging.info("Doing %s %s", "pthard-bin" if isrun else "run", leveltwo)
-            tmppathtwo = os.path.join(tmppath, leveltwo)
-            inputdir= os.path.join(tmppathtwo, trainrun)
-            inputfile = os.path.join(inputdir, targetfile)
-            outputdir = os.path.join(os.path.abspath(outputlocation), "%02d" %pthardbin, "%d" %run)
-            outputfile = os.path.join(outputdir, targetfile)
-            if not os.path.exists(outputfile):
-                TransferFile(inputfile, outputfile)
+    datapool = DataPool()
+    alientool = AlienTool()    
+
+    poolfiller = PoolFiller(sample, trainrun, outputlocation, targetfile, 1000)
+    poolfiller.setdatapool(datapool)
+    poolfiller.setalientool(alientool)
+    poolfiller.start()
+
+    copyworkers = []
+    for i in range(0, 4):
+        worker = CopyHandler()
+        worker.setdatapool(datapool)
+        worker.setpoolfiller(poolfiller)
+        worker.setalienhelper(alientool)
+        worker.setmaxtrials(5)
+        worker.start()
+        copyworkers.append(worker)
+
+    # join all threads
+    poolfiller.join()
+    for worker in workers:
+        worker.join()
+
 
 def usage():
     print "Usage: ./copyFromGrid.py SAMPLE TRAINRUN OUTPUTPATH [OPTIONS]"
--- copyFromGridMeta.py
+++ copyFromGridMeta.py
@@ -5,61 +5,261 @@ import getopt
 import logging
 import md5
 import os
+import subprocess
 import sys
+import threading
 
-def alien_ls(inputpath):
-    dircontent = commands.getstatusoutput("alien_ls %s" %inputpath)[1]
-    directories = dircontent.split("\n")
-    return directories
-
-def alien_cp(inputfile, outputfile):
-    os.system("alien_cp alien://%s %s" %(inputfile, outputfile))
-
-def getchildpaths(inputdir, trainid):
-    childdirs = filter(lambda n : n.split("_")[0] == trainid and "child" in n, alien_ls(inputdir))
-    return map(lambda c : os.path.join(inputdir, c), childdirs)
-
-def getchildnumber(inputpath):
-    if not "child" in inputpath:
-        return 0
-    return int(os.path.basename(inputpath).split("_")[3])
-
-def getmd5local(localfile):
-    md5sum = md5.md5()
-    with open(localfile, "rb") as filereader:
-        data = filereader.read() #read file in chunk and call update on each chunk if file is large.
-        md5sum.update(data)
-    return md5sum.hexdigest()
-
-def getmd5alien(alienfile):
-    return int(commands.getoutput("gbbox md5sum %s | cut -f1" %alienfile)[1], 16)
-
-def checkconsistency(alienfile, localfile):
-    return getmd5alien(alienfile) == getmd5local(localfile)
-
-def main(outputpath, trainname, trainid, filestocopy):
-    outfilenames = filestocopy.split(",")
-    traindir = os.path.join("/alice/cern.ch/user/a/alitrain", trainname)
+class AlienTool: 
     
-    if not os.path.exists(outputpath):
-        os.makedirs(outputpath)
+    def __init__(self):
+        self.__lock = threading.Lock()
+  
+    def md5(self, fname):
+        hash_md5 = hashlib.md5()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+               hash_md5.update(chunk)
+        return hash_md5.hexdigest()
 
-    for childpath in getchildpaths(traindir, trainid):
-        child_id = getchildnumber(childpath)
-        logging.info("Processing child %d", child_id)
+    def gridmd5sum(self, gridfile):
+        # Function must be robust enough to fetch all possible
+        # xrd error states which it usually gets from the stdout
+        # of the query process
+        gbout = ''
+        errorstate = True
+        while errorstate:
+            errorstate = False
+            gbout = commands.getstatusoutput("gbbox md5sum %s" %gridfile)[1]
+            if gbout.startswith("Error") or gbout.startswith("Warning") or "CheckErrorStatus" in gbout:
+                errorstate = True
+        return gbout.split('\t')[0]
 
-        locchilddir = os.path.join(outputpath, "child_%d" %child_id)
-        if not os.path.exists(locchilddir):
-            os.makedirs(locchilddir, 0755)
+    def checkconsistency(self, gridfile, localfile):
+        gmd5 = self.gridmd5sum(gridfile)
+        lmd5 = self.md5(localfile)
+        if lmd5 == gmd5:
+            return True
+        return False
 
-        for transferfile in outfilenames:
-            alienfile = os.path.join(childpath, "merge", transferfile)
-            localfile = os.path.join(locchilddir, transferfile)
-            if os.path.exists(localfile) and checkconsistency(alienfile, localfile):
-                logging.info("Not copying %s because it is already found locally", alienfile)
+    def copy(self, inputfile, outputfile):
+        logging.info("Copying %s to %s", inputfile, outputfile)
+        self.__lock.acquire(True)
+        if not os.path.exists(os.path.dirname(outputfile)):
+            os.makedirs(os.path.dirname(outputfile), 0755)
+        self.__lock.release()
+        subprocess.call(['alien_cp', 'alien://%s'%inputfile, outputfile])
+        # make check
+        if os.path.exists(outputfile):
+            localmd5 = self.md5(outputfile)
+            gridmd5 = self.gridmd5sum(inputfile)
+            if localmd5 != gridmd5:
+                logging.error("Mismatch in MD5 sum for file %s", outputfile)
+                # incorrect MD5sum, outputfile probably corrupted
+                os.remove(outputfile)
+                return False
+            else:
+                logging.info("Output file %s copied correctly", outputfile)
+                return True
+        else:
+            logging.error("output file %s not found" %outputfile)
+            return False
+
+    def listdir(self, inputdir):
+        # Function must be robust agianst error states which
+        # it can only get from the stdout. As long as the 
+        # request ends in error state it should retry
+        errorstate = True
+        while errorstate:
+            dirs = commands.getstatusoutput("alien_ls %s" %inputdir)[1]
+            errorstate = False
+            result = []
+            for d in dirs.split("\n"):
+                if d.startswith("Error") or d.startswith("Warning"):
+                    errorstate = True
+                    break
+                mydir = d.rstrip().lstrip()
+                if len(mydir):
+                    result.append(mydir)
+            if errorstate:
                 continue
-            logging.info("Copying %s to %s", alienfile, localfile);
-            alien_cp(alienfile, localfile)
+            return result
+
+class Filepair:
+  
+    def __init__(self, source, target, ntrials = 0):
+        self.__source = source
+        self.__target = target
+        self.__ntrials = ntrials
+
+    def setntrials(self, ntrials):
+        self.__ntrials = ntrials
+
+    def getntrials(self):
+        return self.__ntrials
+
+    def source(self):
+        return self.__source
+
+    def target(self):
+        return self.__target
+
+class DataPool :
+  
+    def __init__(self):
+        self.__data = []
+        self.__lock = threading.Lock()
+
+    def insert_pool(self, filepair):
+        self.__lock.acquire(True)
+        logging.info("Adding to pool: %s - %s", filepair.source(), filepair.target())
+        self.__data.append(filepair)
+        self.__lock.release()
+
+    def getpoolsize(self):
+        return len(self.__data)
+
+    def pop(self):
+        result = None
+        self.__lock.acquire(True)
+        if(self.getpoolsize()):
+            result = self.__data.pop(0)
+        self.__lock.release()
+        return result
+
+class CopyHandler(threading.Thread):
+  
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.__datapool = None
+        self.__poolfiller = None
+        self.__alienhelper = None
+        self.__maxtrials = 5
+  
+    def setalienhelper(self, alienhelper):
+        self.__alienhelper = alienhelper
+  
+    def setdatapool(self, datapool):
+        self.__datapool = datapool
+
+    def setpoolfiller(self, poolfiller):
+        self.__poolfiller = poolfiller
+
+    def setmaxtrials(self, maxtrials):
+        self.__maxtrials = maxtrials
+
+    def waitforwork(self):
+        if self.__datapool.getpoolsize():
+            return
+        if not self.__poolfiller.isactive():
+            return
+        while not self.__datapool.getpoolsize():
+            if not self.__poolfiller.isactive():
+            break
+        time.sleep(5)
+
+    def run(self):
+        hasWork = True
+        while hasWork:
+            self.waitforwork()
+            nextfile = self.__datapool.pop()
+            if nextfile:
+                copystatus = self.__alienhelper.copy(nextfile.source(), nextfile.target())
+                if not copystatus:
+                    # put file back on the pool in case of copy failure
+                    # only allow for amaximum amount of copy trials
+                    trials = nextfile.getntrials()
+                    trials += 1
+                    if trials >= self.__maxtrials:
+                        logging.error("File %s failed copying in %d trials - giving up", nextfile.source(), self.__maxtrials)
+                    else:
+                        logging.error("File %s failed copying (%d/%d) - re-inserting into the pool ...", nextfile.source(), trials, self.__maxtrials)
+                        nextfile.setntrials(trials)
+                        self.__datapool.insert_pool(nextfile)
+            if not self.__poolfiller.isactive():
+                # if pool is empty exit, else keep thread alive for remaining files
+                if not self.__datapool.getpoolsize():
+                hasWork = False
+
+class PoolFiller(threading.thread):
+    
+    def __init__(self, outputpath, trainname, trainid, filestocopy):
+        threading.Thread.__init__(self)
+        self.__datapool = None
+        self.__alientool = None
+        self.__isacvtive = False
+        self.__outputpath = outputpath
+        self.__trainname = trainname
+        self.__trainid = trainid
+        self.__filestocopy = filestocopy
+
+    def __wait(self):
+        # wait until pool is half empty
+        if self.__datapool.getpoolsize() < self.__maxpoolsize:
+            return
+        # pool full, wait until half empty
+        emptylimit = self.__maxpoolsize/2
+        while self.__datapool.getpoolsize() > emptylimit:
+            time.sleep(5)
+
+    def __getchildpaths(self, inputdir, trainid):
+        childdirs = filter(lambda n : n.split("_")[0] == trainid and "child" in n, self.__alientool.listdir(inputdir))
+        return map(lambda c : os.path.join(inputdir, c), childdirs)
+
+    def getchildnumber(inputpath):
+        if not "child" in inputpath:
+            return 0
+        return int(os.path.basename(inputpath).split("_")[3])
+
+    def run(self):
+        self.__isacvtive = True
+        outfilenames = self.__filestocopy.split(",")
+        traindir = os.path.join("/alice/cern.ch/user/a/alitrain", self.__trainname)
+    
+        if not os.path.exists(self.__outputpath):
+            os.makedirs(self.__outputpath)
+
+        for childpath in self.__getchildpaths(self.__traindir, trainid):
+            child_id = self.__getchildnumber(childpath)
+            logging.info("Processing child %d", child_id)
+
+            locchilddir = os.path.join(self.__outputpath, "child_%d" %child_id)
+            if not os.path.exists(locchilddir):
+                os.makedirs(locchilddir, 0755)
+
+            for transferfile in outfilenames:
+                alienfile = os.path.join(childpath, "merge", transferfile)
+                localfile = os.path.join(locchilddir, transferfile)
+                if os.path.exists(localfile) and self.__alientool.checkconsistency(alienfile, localfile):
+                    logging.info("Not copying %s because it is already found locally", alienfile)
+                    continue
+                logging.info("Copying %s to %s", alienfile, localfile);
+                self.__datapool.insert_pool(Filepair(alienfile, localfile))
+                self.__wait()
+        self.__isacvtive = False
+
+def main(ooutputpath, trainname, trainid, filestocpy):
+    alientool = Alientool()
+    pool = DataPool()
+
+    poolfiller = PoolFiller(ooutputpath, trainname, trainid, filestocpy)
+    poolfiller.setdatapool(datapool)
+    poolfiller.setalientool(alientool)
+    poolfiller.start()
+    
+    copyworkers = []
+    for i in range(0, 4):
+        worker = CopyHandler()
+        worker.setdatapool(datapool)
+        worker.setpoolfiller(poolfiller)
+        worker.setalienhelper(alientool)
+        worker.setmaxtrials(5)
+        worker.start()
+        copyworkers.append(worker)
+
+    # join all threads
+    poolfiller.join()
+    for worker in workers:
+        worker.join()
 
 def usage():
     print "Usage:"
