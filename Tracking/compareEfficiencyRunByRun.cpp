460d30565a1572f5f289b252e02fe76d472eb359
mfasel <markus.fasel@cern.ch>
2018-06-18 19:38:56 +0200

    Adding macro for run-by-run tracking efficiency

--- Tracking/compareEfficiencyRunByRun.cpp
+++ Tracking/compareEfficiencyRunByRun.cpp
@@ -48,66 +48,55 @@ std::set<int> getListOfRuns(const std::string_view inputdir){
   return result;
 }
 
-TH1 *getNormalizedSpectrum(const std::string_view filename, const std::string_view tracktype, const std::string_view trigger, bool isMC, bool restrictEMCAL){
+TH1 *getEfficiency(const std::string_view filename, const std::string_view tracktype, const std::string_view trigger, bool restrictEMCAL){
   std::cout << "Reading " << filename << std::endl;
   try {
     std::unique_ptr<TFile> reader(TFile::Open(filename.data(), "READ"));
     std::stringstream dirbuilder;
     dirbuilder << "ChargedParticleQA_" << tracktype.data();
-    if(!isMC) dirbuilder << "_nocorr";
     auto dir = static_cast<TDirectoryFile *>(reader->Get(dirbuilder.str().data()));
     auto histlist = static_cast<TList *>(static_cast<TKey *>(dir->GetListOfKeys()->At(0))->ReadObj());
     auto norm = static_cast<TH1 *>(histlist->FindObject(Form("hEventCount%s", trigger.data())));
-    auto spec = std::unique_ptr<THnSparse>(static_cast<THnSparse *>(histlist->FindObject(Form("hPtEtaPhiAll%s", trigger.data()))));
+    auto det = std::unique_ptr<THnSparse>(static_cast<THnSparse *>(histlist->FindObject(Form("hPtEtaPhiAll%s", trigger.data())))),
+         part = std::unique_ptr<THnSparse>(static_cast<THnSparse *>(histlist->FindObject("hPtEtaPhiAllTrue")));
     // Look in front of EMCAL
     if(restrictEMCAL){
-      spec->GetAxis(1)->SetRangeUser(-0.6, 0.6);
-      spec->GetAxis(2)->SetRangeUser(1.4, 3.1);
+      det->GetAxis(1)->SetRangeUser(-0.6, 0.6);
+      det->GetAxis(2)->SetRangeUser(1.4, 3.1);
+      det->GetAxis(1)->SetRangeUser(-0.6, 0.6);
+      det->GetAxis(2)->SetRangeUser(1.4, 3.1);
     } else {
-      spec->GetAxis(1)->SetRangeUser(-0.8, 0.8);
+      det->GetAxis(1)->SetRangeUser(-0.8, 0.8);
+      part->GetAxis(1)->SetRangeUser(-0.8, 0.8);
     }
-    auto projected = spec->Projection(0);
-    projected->SetDirectory(nullptr);
-    projected->Scale(1./norm->GetBinContent(1));
-    normalizeBinWidth(projected);
-    return projected;
+    auto projectedDet = det->Projection(0);
+    std::unique_ptr<TH1> projectedPart(part->Projection(0));
+    projectedDet->Sumw2();
+    projectedPart->Sumw2();
+    projectedDet->SetDirectory(nullptr);
+    projectedPart->SetDirectory(nullptr);
+    projectedDet->Divide(projectedDet, projectedPart.get(), 1., 1., "b");
+    return projectedDet;
   } catch (...) {
     std::cout << "Failure ... " << std::endl;
     return nullptr;
   }
 }
 
-double getmin(const TGraphErrors *g){
-  auto result = DBL_MAX;
-  for(auto p : ROOT::TSeqI(g->GetN())) {
-    auto test = g->GetY()[p] - g->GetEY()[p];
-    if(test < result) result = test;
-  }
-  return result;
-}
-
-double getmax(const TGraphErrors *g) {
-  auto result = DBL_MIN;
-  for(auto p : ROOT::TSeqI(g->GetN())) {
-    auto test = g->GetY()[p] + g->GetEY()[p];
-    if(test > result) result = test;
-  }
-  return result;
-}
-
-ROOT6tools::TSavableCanvas *MakePlot(int index, const std::vector<int> &listofruns, const std::string_view inputdir, const std::string_view tracktype, const std::string_view trigger, bool isMC, bool restrictEMCAL, const std::map<double, TGraphErrors *> &trendgraphs){
-  auto plot = new ROOT6tools::TSavableCanvas(Form("TrackComparison_%s_%s_%d", tracktype.data(), trigger.data(), index), Form("Track comparison %s track (%s) %d", tracktype.data(), trigger.data(), index), 800, 600);
+ROOT6tools::TSavableCanvas *MakePlot(int index, const std::vector<int> &listofruns, const std::string_view inputdir, const std::string_view tracktype, const std::string_view trigger, bool restrictEMCAL, const std::map<double, TGraphErrors *> &trendgraphs){
+  std::cout << "Restrincting to EMCAL acceptance: " << (restrictEMCAL ? "True" : "False") << std::endl;
+  auto plot = new ROOT6tools::TSavableCanvas(Form("efficiencyComparison_%s_%s_%d", tracktype.data(), trigger.data(), index), Form("Efficiency comparison %s track (%s) %d", tracktype.data(), trigger.data(), index), 800, 600);
   plot->cd();
-  plot->SetLogy();
 
-  (new ROOT6tools::TAxisFrame(Form("spectrumframe%s%s%d", tracktype.data(), trigger.data(), index), "p_{t} (GeV/c)", "1/N_{trg} dN_{trk}/dp_{t} ((GeV/c)^{-1}", 0., 100., 1e-9, 100.))->Draw("axis");
+  (new ROOT6tools::TAxisFrame(Form("spectrumframe%s%s%d", tracktype.data(), trigger.data(), index), "p_{t} (GeV/c)", "Tracking efficiency", 0., 20., 0, 1.1))->Draw("axis");
   auto trklab =new ROOT6tools::TNDCLabel(0.15, 0.84, 0.5, 0.89, Form("Track type: %s", tracktype.data()));
   trklab->SetTextAlign(12);
   trklab->Draw();
   auto trglab = new ROOT6tools::TNDCLabel(0.15, 0.78, 0.4, 0.83, Form("Trigger: %s", trigger.data()));
   trglab->SetTextAlign(12);
   trglab->Draw();
-  auto leg = new ROOT6tools::TDefaultLegend(0.75, 0.45, 0.89, 0.89);
+  auto leg = new ROOT6tools::TDefaultLegend(0.2, 0.15, 0.65, 0.45);
+  leg->SetNColumns(2);
   leg->Draw();
 
   // Parallelize reading spectra
@@ -116,9 +105,8 @@ ROOT6tools::TSavableCanvas *MakePlot(int index, const std::vector<int> &listofru
   std::mutex specmutex;
   auto workertask = [&](int runnumber) {
     std::string infilename;
-    if(isMC) infilename = Form("%s/%d/AnalysisResults.root", inputdir.data(), runnumber);
-    else infilename = Form("%s/%09d/AnalysisResults.root", inputdir.data(), runnumber);
-    auto spec = getNormalizedSpectrum(infilename.data(), tracktype, trigger, isMC, restrictEMCAL);
+    infilename = Form("%s/%d/AnalysisResults.root", inputdir.data(), runnumber);
+    auto spec = getEfficiency(infilename.data(), tracktype, trigger, restrictEMCAL);
     spec->SetName(Form("%s_%s_%d", trigger.data(), tracktype.data(), runnumber));
     //std::this_thread::sleep_for(std::chrono::duration<double, std::nano>(500));
     std::unique_lock<std::mutex> writelock(specmutex);    // write operation to std::set must be locked
@@ -149,10 +137,9 @@ ROOT6tools::TSavableCanvas *MakePlot(int index, const std::vector<int> &listofru
   return plot;
 }
 
-void compareTracksRunByRun(const std::string_view track_type, const std::string_view trigger, const std::string_view inputdir, bool isMC, bool restrictEMCAL){
+void compareEfficiencyRunByRun(const std::string_view track_type, const std::string_view trigger, const std::string_view inputdir, bool restrictEMCAL){
   ROOT::EnableThreadSafety();
-  std::map<double, TGraphErrors *> trending = {{0.5, new TGraphErrors}, {1., new TGraphErrors}, {2., new TGraphErrors}, 
-                                               {5., new TGraphErrors}, {10., new TGraphErrors}, {20., new TGraphErrors}};
+  std::map<double, TGraphErrors *> trending = {{0.5, new TGraphErrors}, {1., new TGraphErrors}, {2., new TGraphErrors}, {5., new TGraphErrors}};
   auto runs = getListOfRuns(inputdir);
   if(!runs.size()){
     std::cout << "No runs found in input dir " << inputdir << ", skipping ..." << std::endl;
@@ -166,19 +153,19 @@ void compareTracksRunByRun(const std::string_view track_type, const std::string_
   for(auto r : runs) {
     runsplot.emplace_back(r);
     if(runsplot.size() == 10) {
-      auto compplot = MakePlot(canvas++, runsplot, inputdir, track_type, trigger, isMC, restrictEMCAL, trending);
+      auto compplot = MakePlot(canvas++, runsplot, inputdir, track_type, trigger, restrictEMCAL, trending);
       compplot->SaveCanvas(compplot->GetName());
       runsplot.clear();
     }
   }
   if(runsplot.size()){
-    auto compplot = MakePlot(canvas++, runsplot, inputdir, track_type, trigger, isMC, restrictEMCAL, trending);
+    auto compplot = MakePlot(canvas++, runsplot, inputdir, track_type, trigger, restrictEMCAL, trending);
     compplot->SaveCanvas(compplot->GetName());
   }
 
   // Draw trending
-  auto trendplot = new ROOT6tools::TSavableCanvas(Form("trackTrending_%s_%s", track_type.data(), trigger.data()), Form("Track trending %s track %s", track_type.data(), trigger.data()), 1200, 1000);
-  trendplot->Divide(3,2);
+  auto trendplot = new ROOT6tools::TSavableCanvas(Form("efficiencyTrending_%s_%s", track_type.data(), trigger.data()), Form("Efficiency trending %s track %s", track_type.data(), trigger.data()), 1200, 1100);
+  trendplot->Divide(2,2);
   Style trendstyle{kBlack, 20};
 
   std::set<double> ptref;
@@ -189,7 +176,7 @@ void compareTracksRunByRun(const std::string_view track_type, const std::string_
     gPad->SetLeftMargin(0.18);
     gPad->SetRightMargin(0.1);
     auto trendgraph = trending.find(pt)->second;
-    auto frame = new ROOT6tools::TAxisFrame(Form("trend%s%s_pt%d", track_type.data(), trigger.data(), int(pt * 10.)), "run", Form("1/N_{trg} dN/dp_{t}|_{pt = %.1f GeV/c} ((GeV/c)^{-1})", pt), *runs.begin(), *runs.rbegin(), getmin(trendgraph) * 0.9, getmax(trendgraph) * 1.1);
+    auto frame = new ROOT6tools::TAxisFrame(Form("trend%s%s_pt%d", track_type.data(), trigger.data(), int(pt * 10.)), "run", Form("Tracking efficiency|_{pt = %.1f GeV/c}", pt), *runs.begin(), *runs.rbegin(), 0., 1.);
     frame->GetXaxis()->SetNdivisions(305);
     frame->Draw("axis");
     if(ipad == 2){
@@ -206,7 +193,7 @@ void compareTracksRunByRun(const std::string_view track_type, const std::string_
   }
   trendplot->SaveCanvas(trendplot->GetName());
 
-  std::unique_ptr<TFile> writer(TFile::Open(Form("trending_%s_%s.root", track_type.data(), trigger.data()), "RECREATE"));
+  std::unique_ptr<TFile> writer(TFile::Open(Form("trendingEfficiency_%s_%s.root", track_type.data(), trigger.data()), "RECREATE"));
   writer->cd();
   for(auto t : trending){
     auto g = t.second;
