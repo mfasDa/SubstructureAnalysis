38419c8437e8ca503522a1923b72a9ca424c7c7e
mfasel <markus.fasel@cern.ch>
2018-08-27 16:26:18 +0200

    Add meta macro for the Markus Standard Library

---
+++ helpers/msl.C
@@ -0,0 +1,19 @@
+/**
+ * @brief MSL
+ * The Markus Standard Library (helper library for jet substructure analysis)
+ * 
+ * @file msl.C
+ * @author Markus Fasel
+ * @date 2018-08-27
+ */
+#ifndef __MSL_C__
+#define __MSL_C__
+#include "filesystem.C"
+#include "graphics.C"
+#include "math.C"
+#include "pthard.C"
+#include "root.C"
+#include "string.C"
+#include "substructuretree.C"
+#include "unfolding.C"
+#endif // __MSL_C__
