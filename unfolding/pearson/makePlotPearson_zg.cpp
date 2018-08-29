3a38678587407ed04c65b654b6e756ff41ac22a9
mfasel <markus.fasel@cern.ch>
2018-08-29 15:15:15 +0200

    Rename pearson macros for easier handling

--- unfolding/pearson/makePlotPearson_zg.cpp
+++ unfolding/pearson/makePlotPearson_zg.cpp
@@ -1,7 +1,7 @@
 #ifndef __CLING__
 #include <algorithm>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <memory>
 #include <sstream>
 #include <vector>
@@ -33,6 +33,11 @@ struct PtBin {
     }
 };
 
+struct Range {
+    double fMin;
+    double fMax;
+};
+
 std::vector<PtBin> ExtractPtBinning(TFile &reader) {
     reader.cd("iteration4");
     auto hist = static_cast<TH2 *>(gDirectory->Get("zg_unfolded_iter4"));
@@ -42,11 +47,12 @@ std::vector<PtBin> ExtractPtBinning(TFile &reader) {
     return bins;
 } 
 
-void makePlotPearsonZgTemp(std::string_view filename){
+void makePlotPearson_zg(std::string_view filename){
     auto jd = getJetType(getFileTag(filename));
     auto reader = std::unique_ptr<TFile>(TFile::Open(filename.data()));
     auto ptbins = ExtractPtBinning(*reader);
-    auto npanel = ptbins.size(),
+
+    auto npanel =ptbins.size(),
          ncol = npanel / 3 + (npanel % 3 ? 1 : 0);
     std::cout << "Npanel : " << npanel << ",  ncol : " << ncol << std::endl; 
     auto plot = new ROOT6tools::TSavableCanvas(Form("pearsonmatrixzg_%s_R%02d_%s", jd.fJetType.data(), int(jd.fJetRadius * 10.), jd.fTrigger.data()), "plot", 1200, 400 * ncol);
@@ -58,7 +64,7 @@ void makePlotPearsonZgTemp(std::string_view filename){
         bincounter++;
         plot->cd(panel);
         std::stringstream histname;
-        histname << "pearsonmatrix_iter4_binzg" << (bincounter-1);
+        histname << "pearsonmatrix_iter4_binpt" << (bincounter-1);
 
         auto pearsonmatrix  = static_cast<TH2 *>(gDirectory->Get(histname.str().data()));
         pearsonmatrix->SetDirectory(nullptr);
@@ -68,9 +74,10 @@ void makePlotPearsonZgTemp(std::string_view filename){
         pearsonmatrix->SetStats(false);
         pearsonmatrix->Draw("colz");
 
+
         std::stringstream labeltext;
         labeltext.setf(std::ios::fixed, std::ios::floatfield);
-        labeltext << std::setprecision(1) << ptbin.fMin << " GeV/c < p_{t,part} < " << ptbin.fMax << " GeV/c";
+        labeltext << std::setprecision(1) << ptbin.fMin << "  < GeV/c < p_{t,part} < " << ptbin.fMax << " GeV/c";
         (new ROOT6tools::TNDCLabel(0.35, 0.91, 0.89, 0.96, labeltext.str().data()))->Draw();
 
         if(panel==1){
