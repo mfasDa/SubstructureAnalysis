2a80ae0d51f39688ad6f43fa168a3906d93a622f
mfasel <markus.fasel@cern.ch>
2018-09-03 12:21:07 +0200

    Adding meta includes exposing Root/RooUnfold/Root6tools symbols to vscode

---
+++ meta/root6tools.C
@@ -0,0 +1,9 @@
+#ifndef __CLING__
+#include  <TAxisFrame.h>
+#include <TDefaultLegend.h>
+#include <TGraphicsStyle.h>
+#include <TNamedSortable.h>
+#include <TNDCLabel.h>
+#include <TPlotMaker.h>
+#include <TSavableCanvas.h>
+#endif
---
+++ meta/roounfold.C
@@ -0,0 +1,15 @@
+#ifndef __CLING__
+#include <RooUnfoldBayes.h>
+#include <RooUnfoldErrors.h>
+#include <RooUnfoldInvert.h>
+#include <RooUnfoldParms.h>
+#include <RooUnfoldSvd.h>
+#include <RooUnfoldUtils.h>
+#include <TSVDUnfold_local.h>
+#include <RooUnfoldBinByBin.h>
+#include <RooUnfold.h>
+#include <RooUnfold_LinkDef.h>
+#include <RooUnfoldResponse.h>
+#include <RooUnfoldTUnfold.h>
+#include <TauSVDUnfold.h>
+#endif
