f6bfa5b47c9c723b633e1b4d92f87b56e4868ba2
Markus Fasel <markus.fasel@cern.ch>
2019-04-26 18:03:01 +0200

    Defining default constructors

--- struct/Ratio.cxx
+++ struct/Ratio.cxx
@@ -5,11 +5,13 @@
 
 class Ratio : public TH1D{
 public:
+    Ratio() = default;
     Ratio(const TH1 *numerator, const TH1 *denominator, Option_t *option = "") : TH1D(*static_cast<const TH1D *>(numerator)) {
         SetName(Form("ratio_%s_%s", numerator->GetName(), denominator->GetName())); 
         if(strlen(option)) Divide(numerator, denominator, 1., 1., option);
         else Divide(denominator);
         SetDirectory(nullptr);
     }
+    virtual ~Ratio() = default;
 };
 #endif
