ad340661daa78b11a733294b9d117e2ec71d879b
Markus Fasel <markus.fasel@cern.ch>
2019-04-29 13:11:08 +0200

    Adapting comparison macros in restricted range to new struct classes

--- unfolding/comparisons/ultra/makeComparisonUnfoldedRestricted.cpp
+++ unfolding/comparisons/ultra/makeComparisonUnfoldedRestricted.cpp
@@ -1,87 +1,47 @@
+#include "../../../meta/stl.C"
+#include "../../../meta/root.C"
+#include "../../../meta/root6tools.C"
 #include "../../binnings/binningPt1D.C"
 #include "../../../helpers/graphics.C"
 #include "../../../helpers/math.C"
-
-std::map<double, TH1 *> readRawSpectra(const std::string_view filename){
-    auto binningtmp = getJetPtBinningNonLinTrueLargeFineLow();
-    std::vector<double> binning;
-    // restrict to reported range of the measurement with the regular binning
-    for(auto b : binningtmp) {
-        if(b < 20.) continue;
-        if(b > 240.) break;
-        binning.push_back(b);
-    }
-    TH1D histtemplate("histtemplate", "histtemplate", binning.size()-1, binning.data());
-    std::map<double, TH1 *> result;
-    std::unique_ptr<TFile> reader(TFile::Open(filename.data(), "READ"));
-    for(double r = 0.2; r < 0.7; r+= 0.1) {
-        std::string rstring = Form("R%02d", int(r*10.));
-        reader->cd(rstring.data());
-        gDirectory->cd("reg4");
-        auto spectrumtmp = static_cast<TH1 *>(gDirectory->Get("normalized_reg4"));
-        auto spectrum = new TH1D(histtemplate);
-        spectrum->SetDirectory(nullptr);
-        spectrum->SetNameTitle(spectrumtmp->GetName(), spectrumtmp->GetTitle());
-        for(auto b : ROOT::TSeqI(0, spectrum->GetXaxis()->GetNbins())) {
-            auto tmpbin = spectrumtmp->GetXaxis()->FindBin(spectrum->GetXaxis()->GetBinCenter(b+1));
-            spectrum->SetBinContent(b+1, spectrumtmp->GetBinContent(tmpbin));
-            spectrum->SetBinError(b+1, spectrumtmp->GetBinError(tmpbin));
-        }
-        result[r] = spectrum;
-    }
-    return result;
-}
+#include "../../../struct/GraphicsPad.cxx"
+#include "../../../struct/JetSpectrumReader.cxx"
+#include "../../../struct/Restrictor.cxx"
+#include "../../../struct/Ratio.cxx"
 
 void makeComparisonUnfoldedRestricted(const std::string_view sysvar){
-    auto dataUltra = readRawSpectra(Form("correctedSVD_ultra240_%s.root", sysvar.data())), datareg = readRawSpectra(Form("correctedSVD_fine_lowpt_%s.root", sysvar.data()));
-
-    auto plot = new ROOT6tools::TSavableCanvas(Form("comparisonUnfoldedRanges%s", sysvar.data()), "Comparison raw spectra in identical range", 300 * dataUltra.size(), 700);
-    plot->Divide(dataUltra.size(),2);
+    std::vector<std::string> spectra = {"normalized_reg4"};
+    auto dataUltra = JetSpectrumReader(Form("correctedSVD_ultra240_%s.root", sysvar.data()), spectra), 
+         dataReg = JetSpectrumReader(Form("correctedSVD_fine_lowpt_%s.root", sysvar.data()), spectra);
+    auto jetradii = dataUltra.GetJetSpectra().GetJetRadii();
+    Restrictor reported(20., 240.);
+    
+    auto plot = new ROOT6tools::TSavableCanvas(Form("comparisonUnfoldedRanges%s", sysvar.data()), "Comparison raw spectra in identical range", 300 * jetradii.size(), 700);
+    plot->Divide(jetradii.size(),2);
 
     Style ultrastyle{kRed, 24}, regstyle{kBlue, 25}, ratiostyle{kBlack, 20};
     int icol = 0;
-    for(auto ultrait = dataUltra.begin(), regit = datareg.begin(); ultrait != dataUltra.end(); ++ultrait, ++regit){
+    for(auto r : jetradii){
         plot->cd(icol+1);
-        gPad->SetTopMargin(0.02);
-        gPad->SetLeftMargin(0.15);
-        gPad->SetRightMargin(0.02);
         gPad->SetLogy();
-        auto specframe = new ROOT6tools::TAxisFrame(Form("specframeR%02d", int(ultrait->first * 10.)), "p_{t} (GeV/c)", "dN/dp_{t}", 0., 250, 1e-7, 1e-1);
-        specframe->GetXaxis()->SetTitleSize(0.045);
-        specframe->GetXaxis()->SetLabelSize(0.045);
-        specframe->GetYaxis()->SetTitleSize(0.045);
-        specframe->GetYaxis()->SetLabelSize(0.045);
-        specframe->Draw("axis");
-        (new ROOT6tools::TNDCLabel(0.25, 0.15, 0.4, 0.22, Form("R=%.1f", ultrait->first)))->Draw();
-        TLegend *leg(nullptr);
-        if(!icol){
-            leg = new ROOT6tools::TDefaultLegend(0.6, 0.7, 0.95, 0.95);
-            leg->SetTextSize(0.045);
-            leg->Draw();
-        }
-        ultrastyle.SetStyle<TH1>(*ultrait->second);
-        regstyle.SetStyle<TH1>(*regit->second);
-        ultrait->second->Draw("epsame");
-        regit->second->Draw("epsame");
-        if(leg) {
-            leg->AddEntry(regit->second, "Normal range", "lep" );
-            leg->AddEntry(ultrait->second, "Wide range", "lep" );
-        }
-        plot->cd(icol+dataUltra.size()+1);
-        gPad->SetTopMargin(0.02);
-        gPad->SetLeftMargin(0.15);
-        gPad->SetRightMargin(0.02);
-        auto ratioframe = new ROOT6tools::TAxisFrame(Form("specframeR%02d", int(ultrait->first * 10.)), "p_{t} (GeV/c)", "wide/normal", 0., 250, 0.8, 1.2); 
-        ratioframe->GetXaxis()->SetTitleSize(0.045);
-        ratioframe->GetXaxis()->SetLabelSize(0.045);
-        ratioframe->GetYaxis()->SetTitleSize(0.045);
-        ratioframe->GetYaxis()->SetLabelSize(0.045);
-        ratioframe->Draw("axis");
-        auto ratio = static_cast<TH1 *>(ultrait->second->Clone(Form("ratioR%02d", int(ultrait->first * 10.))));
-        ratio->SetDirectory(nullptr);
-        ratio->Divide(regit->second);
-        ratiostyle.SetStyle<TH1>(*ratio);
-        ratio->Draw("epsame");
+        GraphicsPad specpad(gPad);
+        specpad.Margins(0.17, 0.04, -1., 0.02);
+        specpad.Frame(Form("specframeR%02d", int(r * 10.)), "p_{t} (GeV/c)", "d#sigma/dp_{t}d#eta (mb/(GeV/c))", 0., 250, 1e-7, 1e-2);
+        specpad.FrameTextSize(0.047);
+        specpad.Label(0.25, 0.15, 0.4, 0.22, Form("R=%.1f", r));
+        if(!icol) specpad.Legend(0.3, 0.7, 0.93, 0.93);
+        auto specultra = reported(dataUltra.GetJetSpectrum(r, spectra[0])),
+             specreg = reported(dataReg.GetJetSpectrum(r, spectra[0]));
+        specpad.Draw<TH1>(specreg, regstyle, "10 GeV/c < p_{t,j,det} < 200 GeV/c");
+        specpad.Draw<TH1>(specultra, ultrastyle, "10 GeV/c < p_{t,j,det} < 240 GeV/c");
+
+        plot->cd(icol+jetradii.size()+1);
+        GraphicsPad ratiopad(gPad);
+        ratiopad.Margins(0.17, 0.04, -1., 0.02);
+        ratiopad.Frame(Form("specframeR%02d", int(r * 10.)), "p_{t} (GeV/c)", "large range /vormal range", 0., 250, 0.8, 1.2);
+        ratiopad.FrameTextSize(0.047);
+        Ratio *specratio = new Ratio(specultra,specreg);
+        ratiopad.Draw<Ratio>(specratio, ratiostyle);
         icol++;
     }
     plot->cd();
--- unfolding/comparisons/ultra/makeComparisonUnfoldedRestrictedHigh.cpp
+++ unfolding/comparisons/ultra/makeComparisonUnfoldedRestrictedHigh.cpp
@@ -1,91 +1,50 @@
+#include "../../../meta/stl.C"
+#include "../../../meta/root.C"
+#include "../../../meta/root6tools.C"
 #include "../../binnings/binningPt1D.C"
 #include "../../../helpers/graphics.C"
 #include "../../../helpers/math.C"
-
-std::map<double, TH1 *> readRawSpectra(const std::string_view filename){
-    auto binningtmp = getJetPtBinningNonLinTrueUltra240();
-    std::vector<double> binning;
-    // restrict to reported range of the measurement with the regular binning
-    for(auto b : binningtmp) {
-        if(b < 20.) continue;
-        if(b > 321.) break;
-        binning.push_back(b);
-    }
-    TH1D histtemplate("histtemplate", "histtemplate", binning.size()-1, binning.data());
-    std::map<double, TH1 *> result;
-    std::unique_ptr<TFile> reader(TFile::Open(filename.data(), "READ"));
-    for(double r = 0.2; r <= 0.6; r+= 0.1) {
-        std::string rstring = Form("R%02d", int(r*10.));
-        reader->cd(rstring.data());
-        gDirectory->cd("reg4");
-        auto spectrumtmp = static_cast<TH1 *>(gDirectory->Get("normalized_reg4"));
-        auto spectrum = new TH1D(histtemplate);
-        spectrum->SetDirectory(nullptr);
-        spectrum->SetNameTitle(spectrumtmp->GetName(), spectrumtmp->GetTitle());
-        for(auto b : ROOT::TSeqI(0, spectrum->GetXaxis()->GetNbins())) {
-            auto tmpbin = spectrumtmp->GetXaxis()->FindBin(spectrum->GetXaxis()->GetBinCenter(b+1));
-            spectrum->SetBinContent(b+1, spectrumtmp->GetBinContent(tmpbin));
-            spectrum->SetBinError(b+1, spectrumtmp->GetBinError(tmpbin));
-        }
-        result[r] = spectrum;
-    }
-    return result;
-}
+#include "../../../struct/GraphicsPad.cxx"
+#include "../../../struct/JetSpectrumReader.cxx"
+#include "../../../struct/Restrictor.cxx"
+#include "../../../struct/Ratio.cxx"
 
 void makeComparisonUnfoldedRestrictedHigh(const std::string_view sysvar = "notc"){
-    auto data300 = readRawSpectra(Form("correctedSVD_ultra300_%s.root", sysvar.data())), data240 = readRawSpectra(Form("correctedSVD_ultra240_%s.root", sysvar.data()));
+    std::vector<std::string> spectra = {"normalized_reg4"};
+    auto dataUltra = JetSpectrumReader(Form("correctedSVD_ultra240_%s.root", sysvar.data()), spectra), 
+         dataReg = JetSpectrumReader(Form("correctedSVD_ultra300_%s.root", sysvar.data()), spectra);
+    auto jetradii = dataUltra.GetJetSpectra().GetJetRadii();
+    Restrictor reported(20., 320.);
 
-    auto plot = new ROOT6tools::TSavableCanvas(Form("comparisonUnfoldedRangesHigh%s", sysvar.data()), "Comparison raw spectra in identical range", 300 * data300.size(), 700);
-    plot->Divide(data300.size(),2);
+    auto plot = new ROOT6tools::TSavableCanvas(Form("comparisonUnfoldedRangesHigh%s", sysvar.data()), "Comparison raw spectra in identical range", 300 * jetradii.size(), 700);
+    plot->Divide(jetradii.size(),2);
 
-    Style style300{kRed, 24}, style240{kBlue, 25}, ratiostyle{kBlack, 20};
+    Style ultrastyle{kRed, 24}, regstyle{kBlue, 25}, ratiostyle{kBlack, 20};
     int icol = 0;
-    for(auto it300 = data300.begin(), it240 = data240.begin(); it300 != data300.end(); ++it300, ++it240){
+    for(auto r : jetradii){
         plot->cd(icol+1);
-        gPad->SetTopMargin(0.02);
-        gPad->SetLeftMargin(0.16);
-        gPad->SetRightMargin(0.02);
         gPad->SetLogy();
-        auto specframe = new ROOT6tools::TAxisFrame(Form("specframeR%02d", int(it300->first * 10.)), "p_{t} (GeV/c)", "d#sigma/(dp_{t}d#eta) (mb/(GeV/c))", 0., 350, 1e-9, 1e-1);
-        specframe->GetXaxis()->SetTitleSize(0.045);
-        specframe->GetXaxis()->SetLabelSize(0.045);
-        specframe->GetYaxis()->SetTitleSize(0.045);
-        specframe->GetYaxis()->SetLabelSize(0.045);
-        specframe->Draw("axis");
-        (new ROOT6tools::TNDCLabel(0.25, 0.15, 0.4, 0.22, Form("R=%.1f", it300->first)))->Draw();
-        TLegend *leg(nullptr);
-        if(!icol){
-            leg = new ROOT6tools::TDefaultLegend(0.4, 0.7, 0.95, 0.95);
-            leg->SetTextSize(0.045);
-            leg->Draw();
-        }
+        GraphicsPad specpad(gPad);
+        specpad.Margins(0.17, 0.04, -1., 0.02);
+        specpad.Frame(Form("specframeR%02d", int(r * 10.)), "p_{t} (GeV/c)", "d#sigma/dp_{t}d#eta (mb/(GeV/c))", 0., 350, 1e-9, 1e-2);
+        specpad.FrameTextSize(0.047);
+        specpad.Label(0.25, 0.15, 0.4, 0.22, Form("R=%.1f", r));
+        if(!icol) specpad.Legend(0.3, 0.7, 0.93, 0.93);
+        auto specultra = reported(dataUltra.GetJetSpectrum(r, spectra[0])),
+             specreg = reported(dataReg.GetJetSpectrum(r, spectra[0]));
+        specpad.Draw<TH1>(specreg, regstyle, "10 GeV/c < p_{t,j,det} < 240 GeV/c");
+        specpad.Draw<TH1>(specultra, ultrastyle, "10 GeV/c < p_{t,j,det} < 320 GeV/c");
 
-        style300.SetStyle<TH1>(*it300->second);
-        style240.SetStyle<TH1>(*it240->second);
-        it300->second->Draw("epsame");
-        it240->second->Draw("epsame");
-        if(leg) {
-            leg->AddEntry(it240->second, "p_{t,det} < 240 GeV/c", "lep" );
-            leg->AddEntry(it300->second, "p_{t,det} < 300 GeV/c", "lep" );
-        }
-        plot->cd(icol+data300.size()+1);
-        gPad->SetTopMargin(0.02);
-        gPad->SetLeftMargin(0.16);
-        gPad->SetRightMargin(0.02);
-        auto ratioframe = new ROOT6tools::TAxisFrame(Form("specframeR%02d", int(it300->first * 10.)), "p_{t} (GeV/c)", "(p_{t,det} < 300 GeV/c)/(p_{t,det} < 240 GeV/c)", 0., 350, 0.8, 1.2); 
-        ratioframe->GetXaxis()->SetTitleSize(0.045);
-        ratioframe->GetXaxis()->SetLabelSize(0.045);
-        ratioframe->GetYaxis()->SetTitleSize(0.045);
-        ratioframe->GetYaxis()->SetLabelSize(0.045);
-        ratioframe->Draw("axis");
-        auto ratio = static_cast<TH1 *>(it300->second->Clone(Form("ratioR%02d", int(it300->first * 10.))));
-        ratio->SetDirectory(nullptr);
-        ratio->Divide(it240->second);
-        ratiostyle.SetStyle<TH1>(*ratio);
-        ratio->Draw("epsame");
+        plot->cd(icol+jetradii.size()+1);
+        GraphicsPad ratiopad(gPad);
+        ratiopad.Margins(0.17, 0.04, -1., 0.02);
+        ratiopad.Frame(Form("specframeR%02d", int(r * 10.)), "p_{t} (GeV/c)", "large range /vormal range", 0., 350, 0.8, 1.2);
+        ratiopad.FrameTextSize(0.047);
+        Ratio *specratio = new Ratio(specultra,specreg);
+        ratiopad.Draw<Ratio>(specratio, ratiostyle);
         icol++;
     }
     plot->cd();
     plot->Update();
-    plot->SaveCanvas(plot->GetName());
+    plot->SaveCanvas(plot->GetName());;
 }
