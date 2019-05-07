9115f4f211106cc96078e8413a4e28372bc0d7f0
Markus Fasel <markus.fasel@cern.ch>
2019-05-07 10:29:46 +0200

    Add systematics for binning variation

--- unfolding/binnings/binningPt1D_bv.C
+++ unfolding/binnings/binningPt1D_bv.C
@@ -1,35 +1,51 @@
 #include "../../meta/stl.C"
 #include "../../meta/root.C"
 
+std::vector<double> getJetPtBinningNonLinSmearOption1(){
+  return { 10., 11., 12., 13., 14., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33, 36., 39., 42., 46., 52., 58., 68., 78., 88., 100., 115., 140.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearOption2(){
+  return { 10., 12., 14., 16., 18., 20., 22., 24., 26., 28., 31., 34., 37., 40., 45., 50., 56., 60., 65., 71., 77., 85., 93., 103., 113., 125., 140.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearOption3(){
+  return { 10., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 41., 46., 51., 56., 61., 71., 81., 91., 101., 111., 121., 140.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearOption4(){
+  return { 10., 11., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 44., 49., 54., 59., 69., 79., 89., 99., 109., 119., 140.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearBV(const std::string_view option){
+  std::unordered_map<std::string, std::function<std::vector<double>()>> functors = {
+      {"option1", getJetPtBinningNonLinSmearOption1},  
+      {"option2", getJetPtBinningNonLinSmearOption2},
+      {"option3", getJetPtBinningNonLinSmearOption3},
+      {"option4", getJetPtBinningNonLinSmearOption4}
+  };
+  auto functor = functors.find(std::string(option));
+  if(functor == functors.end()) return {};
+  return functor->second(); 
+}
+
 std::vector<double> getJetPtBinningNonLinSmearLargeOption1(){
-  // default binning:  {20., 22., 24., 26., 28., 30., 32., 34., 36., 38., 40., 45., 50., 
-  //                    55., 60., 70., 80., 90., 100., 110., 120., 140., 160., 180. 200.};
-  return {20., 21.5, 23., 24.5, 26., 27.5, 29., 30.5, 33., 34.5, 36., 40., 44., 
-          48., 52., 60., 68., 76., 84., 92., 104., 119., 144., 172., 200.};
+  return { 10., 11., 12., 13., 14., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 36., 39., 42., 46., 52., 58., 68., 78., 88., 98., 108., 125., 145., 170., 200.};
 }
 
 std::vector<double> getJetPtBinningNonLinSmearLargeOption2(){
-  // default binning:  {20., 22., 24., 26., 28., 30., 32., 34., 36., 38., 40., 45., 50., 
-  //                    55., 60., 70., 80., 90., 100., 110., 120., 140., 160., 180. 200.};
-  return {20., 22.5, 25., 27.5, 30., 32.5, 35., 37.5, 40., 42.5, 45., 52., 59., 
-          56., 63., 74., 85., 96., 107., 118., 132., 149., 166., 183., 200.};
+  return { 10., 12., 14., 16., 18., 20., 22., 25., 28., 31., 34., 38., 42., 47., 52., 58., 64., 71., 78., 86., 94., 105., 114., 123., 133., 144., 155., 167., 183., 200.};
 }
 
 std::vector<double> getJetPtBinningNonLinSmearLargeOption3(){
-  // default binning:  {20., 22., 24., 26., 28., 30., 32., 34., 36., 38., 40., 45., 50., 
-  //                    55., 60., 70., 80., 90., 100., 110., 120., 140., 160., 180., 200.};
- return {20., 23., 25., 27., 29., 31., 33., 35., 37., 39., 41., 46., 51., 56., 61., 71., 81., 
-         91., 101., 111., 121., 141., 161., 181., 200.};
+  return { 10., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 41., 46., 51., 56., 61., 71., 81., 91., 101., 111., 121., 141., 161., 181., 200.};
 }
 
 std::vector<double> getJetPtBinningNonLinSmearLargeOption4(){
-  // default binning:  {20., 22., 24., 26., 28., 30., 32., 34., 36., 38., 40., 45., 50., 
-  //                    55., 60., 70., 80., 90., 100., 110., 120., 140., 160., 180., 200.};
-  return {20., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 44., 49., 54., 59., 69., 79., 89., 
-          99., 109., 119., 139., 159., 179., 200.};
+  return { 10., 11., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 44., 49., 54., 59., 69., 79., 89., 99., 109., 119., 139., 159., 179., 200.};
 }
 
-std::vector<double> getJetPtBinningNonLinSmearLarge(const std::string_view option){
+std::vector<double> getJetPtBinningNonLinSmearLargeBV(const std::string_view option){
     std::unordered_map<std::string, std::function<std::vector<double>()>> functors = {
       {"option1", getJetPtBinningNonLinSmearLargeOption1},  
       {"option2", getJetPtBinningNonLinSmearLargeOption2},
@@ -41,23 +57,86 @@ std::vector<double> getJetPtBinningNonLinSmearLarge(const std::string_view optio
     return functor->second();
 }
 
-std::vector<double> getJetPtBinningNonLinTrueLarge(){
-  std::vector<double> result;
-  result.emplace_back(0.);
-  result.emplace_back(20.);
-  double current = 20.;
-  while(current < 80.) {
-    current += 10.;
-    result.push_back(current);
-  }
-  while(current < 200){
-    current += 20;
-    result.push_back(current);
-  }
-  while(current < 280.){
-    current += 40.;
-    result.push_back(current);
-  }
-  result.emplace_back(600.);
-  return result;
+std::vector<double> getJetPtBinningNonLinSmearLargeFineOption1(){
+  return { 10., 11., 12., 13., 14., 16., 17., 19., 21., 23., 25., 27., 29., 32., 35., 38., 41., 44., 47., 50., 53., 56., 59., 62., 65., 69., 73., 78., 85., 92., 99., 106., 114., 122., 131., 139., 148., 158., 170., 184., 200.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearLargeFineOption2(){
+  return { 10., 12., 14., 16., 18., 20., 22., 24., 26., 28., 31., 34., 37., 40., 44., 48., 52., 56., 60., 65., 70., 75., 80., 85., 90., 95., 100., 106., 112., 118., 124., 131., 138., 145., 152., 160., 168., 176., 184., 192., 200.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearLargeFineOption3(){
+  return { 10., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 41., 45., 49., 53., 57., 61., 66., 71., 76., 81., 86., 91., 96., 101., 106., 111., 116., 121., 131., 141., 151., 161., 171., 181., 191., 200.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearLargeFineOption4(){
+  return { 10., 11., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 43., 47., 51., 55., 59., 64., 69., 74., 79., 84., 89., 94., 99., 104., 109., 114., 119., 129., 139., 149., 159., 169., 179., 189., 200.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearLargeFineBV(const std::string_view option){
+    std::unordered_map<std::string, std::function<std::vector<double>()>> functors = {
+      {"option1", getJetPtBinningNonLinSmearLargeFineOption1},  
+      {"option2", getJetPtBinningNonLinSmearLargeFineOption2},
+      {"option3", getJetPtBinningNonLinSmearLargeFineOption3},
+      {"option4", getJetPtBinningNonLinSmearLargeFineOption4}
+    };
+    auto functor = functors.find(std::string(option));
+    if(functor == functors.end()) return {};
+    return functor->second();
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra240Option1(){
+  return { 10., 11., 12., 13., 14., 15., 17., 19., 21., 23., 25., 27., 29., 32., 35., 38., 41., 44., 47., 50., 53., 56., 59., 62., 65., 69., 73., 78., 85., 92., 99., 106., 114., 122., 131., 139., 148., 158., 170., 184., 200., 218., 240.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra240Option2(){
+  return { 10., 12., 14., 16., 18., 20., 22., 24., 26., 28., 31., 34., 37., 40., 44., 48., 52., 56., 60., 65., 70., 75., 80., 85., 90., 95., 100., 106., 112., 118., 124., 131., 138., 145., 152., 160., 168., 176., 184., 194., 207., 222., 240.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra240Option3(){
+  return { 10., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 41., 45., 49., 53., 57., 61., 66., 71., 76., 81., 86., 91., 96., 101., 106., 111., 116., 121., 131., 141., 151., 161., 171., 181., 191., 201., 221., 240.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra240Option4(){
+  return { 10., 11., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 43., 47., 51., 55., 59., 64., 69., 74., 79., 84., 89., 94., 99., 104., 109., 114., 119., 129., 139., 149., 159., 169., 179., 189., 199., 219., 240.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra240BV(const std::string_view option){
+    std::unordered_map<std::string, std::function<std::vector<double>()>> functors = {
+      {"option1", getJetPtBinningNonLinSmearUltra240Option1},  
+      {"option2", getJetPtBinningNonLinSmearUltra240Option2},
+      {"option3", getJetPtBinningNonLinSmearUltra240Option3},
+      {"option4", getJetPtBinningNonLinSmearUltra240Option4}
+    };
+    auto functor = functors.find(std::string(option));
+    if(functor == functors.end()) return {};
+    return functor->second();
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra300Option1(){
+  return { 10., 11., 12., 13., 14., 15., 17., 19., 21., 23., 25., 27., 29., 32., 35., 38., 41., 44., 47., 50., 53., 56., 59., 62., 65., 69., 73., 78., 85., 92., 99., 106., 114., 122., 131., 139., 148., 158., 170., 184., 200., 218., 240., 260., 280, 300.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra300Option2(){
+  return { 10., 12., 14., 16., 18., 20., 22., 24., 26., 28., 31., 34., 37., 40., 44., 48., 52., 56., 60., 65., 70., 75., 80., 85., 90., 95., 100., 106., 112., 118., 124., 131., 138., 145., 152., 160., 168., 176., 184., 194., 210., 228., 246., 264., 282., 300.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra300Option3(){
+  return { 10., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 41., 45., 49., 53., 57., 61., 66., 71., 76., 81., 86., 91., 96., 101., 106., 111., 116., 121., 131., 141., 151., 161., 171., 181., 191., 201., 221., 241., 261., 281., 300.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra300Option4(){
+  return { 10., 11., 13., 15., 17., 19., 21., 23., 25., 27., 29., 31., 33., 35., 37., 39., 43., 47., 51., 55., 59., 64., 69., 74., 79., 84., 89., 94., 99., 104., 109., 114., 119., 129., 139., 149., 159., 169., 179., 189., 199., 219., 240.};
+}
+
+std::vector<double> getJetPtBinningNonLinSmearUltra300BV(const std::string_view option){
+    std::unordered_map<std::string, std::function<std::vector<double>()>> functors = {
+      {"option1", getJetPtBinningNonLinSmearUltra300Option1},  
+      {"option2", getJetPtBinningNonLinSmearUltra300Option2},
+      {"option3", getJetPtBinningNonLinSmearUltra300Option3},
+      {"option4", getJetPtBinningNonLinSmearUltra300Option4}
+    };
+    auto functor = functors.find(std::string(option));
+    if(functor == functors.end()) return {};
+    return functor->second();
 }
---
+++ unfolding/runCorrectionChain1DBayes_SpectrumTaskSimpleFineLow_SysBinVar.cpp
@@ -0,0 +1,326 @@
+#include "../meta/stl.C"
+#include "../meta/root.C"
+#include "../meta/roounfold.C"
+#include "../helpers/math.C"
+#include "../helpers/unfolding.C"
+#include "binnings/binningPt1D.C"
+#include "binnings/binningPt1D_bv.C"
+
+struct unfoldingResults {
+    int fReg;
+    TH1 *fUnfolded;
+    TH1 *fNormalized;
+    TH1 *fBackfolded;
+    TH1 *fUnfoldedClosure;
+    TH1 *fDvector;
+    TH1 *fDvectorClosure;
+    TH2 *fPearson;
+    TH2 *fPearsonClosure;
+
+    bool operator<(const unfoldingResults &other) const { return fReg < other.fReg; }
+    bool operator==(const unfoldingResults &other) const { return fReg == other.fReg; }
+};
+
+struct UnfoldingConfiguration {
+    int fReg;
+    double fRadius;
+    TH1 *fRaw;
+    RooUnfoldResponse *fResponseMatrix;
+    TH1 *fDetLevelClosure;
+    RooUnfoldResponse *fResponseMatrixClosure;
+};
+
+class UnfoldingPool {
+public:
+    UnfoldingPool() = default;
+    ~UnfoldingPool() = default;
+
+    void InsertWork(UnfoldingConfiguration config) {
+        std::lock_guard<std::mutex> insert_lock(fAccessLock);
+        fData.push(config);
+    }
+
+    UnfoldingConfiguration next() {
+        std::lock_guard<std::mutex> pop_lock(fAccessLock);
+        UnfoldingConfiguration result = fData.front();
+        fData.pop();
+        return result;
+    }
+
+    bool empty() { return fData.empty(); }
+
+private:
+    std::mutex                              fAccessLock;
+    std::queue<UnfoldingConfiguration>      fData;              
+};
+
+class UnfoldingRunner {
+    public:
+        UnfoldingRunner(UnfoldingPool *work) : fOutputData(), fInputData(work) {}
+        ~UnfoldingRunner() = default;
+
+        void DoWork() {
+            while(!fInputData->empty()) {
+                fOutputData.push_back(runUnfolding(fInputData->next()));
+            }
+        }
+
+        const std::vector<unfoldingResults> &getUnfolded() const { return fOutputData; }
+
+    private:
+        unfoldingResults runUnfolding(const UnfoldingConfiguration &config){
+            RooUnfold::ErrorTreatment errorTreatment = RooUnfold::kCovToy;
+            const double kSizeEmcalPhi = 1.88,
+                         kSizeEmcalEta = 1.4;
+            auto acceptance = (kSizeEmcalPhi - 2 * config.fRadius) * (kSizeEmcalEta - 2 * config.fRadius) / (TMath::TwoPi());
+            std::cout << "[Bayes unfolding] Regularization " << config.fReg << "\n================================================================\n";
+            std::cout << "[Bayes unfolding] Running unfolding" << std::endl;
+            RooUnfoldBayes unfolder(config.fResponseMatrix, config.fRaw, config.fReg);
+            auto specunfolded = unfolder.Hreco(errorTreatment);
+            specunfolded->SetNameTitle(Form("unfolded_reg%d", config.fReg), Form("Unfolded jet spectrum R=%.1f reg %d", config.fRadius, config.fReg));
+            specunfolded->SetDirectory(nullptr);
+            auto backfolded = MakeRefolded1D(config.fRaw, specunfolded, *config.fResponseMatrix);
+            backfolded->SetNameTitle(Form("backfolded_reg%d", config.fReg), Form("back-folded jet spectrum R=%.1f reg %d", config.fRadius, config.fReg));
+            backfolded->SetDirectory(nullptr);
+            specunfolded->Scale(1., "width");
+            auto specnormalized = static_cast<TH1 *>(specunfolded->Clone(Form("normalizedReg%d", config.fReg)));
+            specnormalized->SetNameTitle(Form("normalized_reg%d", config.fReg), Form("Normalized jet spectrum R=%.1f reg %d", config.fRadius, config.fReg));
+            specnormalized->SetDirectory(nullptr);
+            specnormalized->Scale(1. / (acceptance));
+
+            // run closure test
+            std::cout << "[Bayes unfolding] Running closure test" << std::endl;
+            RooUnfoldBayes unfolderClosure(config.fResponseMatrixClosure, config.fDetLevelClosure, config.fReg);
+            auto specunfoldedClosure = unfolderClosure.Hreco(errorTreatment);
+            specunfoldedClosure->SetDirectory(nullptr);
+            specunfoldedClosure->SetNameTitle(Form("unfoldedClosure_reg%d", config.fReg), Form("Unfolded jet spectrum of the closure test R=%.1f reg %d", config.fRadius, config.fReg));
+            specunfoldedClosure->Scale(1., "width");
+            return {config.fReg, specunfolded, specnormalized, backfolded, specunfoldedClosure, nullptr, nullptr, 
+                    CorrelationHist1D(unfolder.Ereco(), Form("PearsonReg%d", config.fReg), Form("Pearson coefficients regularization %d", config.fReg)),
+                    CorrelationHist1D(unfolderClosure.Ereco(), Form("PearsonClosureReg%d", config.fReg), Form("Pearson coefficients of the closure test regularization %d", config.fReg))};
+        }
+
+        std::vector<unfoldingResults>   fOutputData;       
+        UnfoldingPool                   *fInputData;
+};
+
+std::pair<double, TH1 *> getSpectrumAndNorm(TFile &reader, double R, const std::string_view trigger, const std::string_view triggercluster, const std::string_view sysvar) {
+    int clusterbin = 0;
+    if(triggercluster == "ANY") clusterbin = 1;
+    else if(triggercluster == "CENT") clusterbin = 2;
+    else if(triggercluster == "CENTNOTRD") clusterbin = 3;
+    std::stringstream dirnamebuilder, histnamebuilder;
+    dirnamebuilder << "JetSpectrum_FullJets_R" << std::setw(2) << std::setfill('0') << int(R*10.) << "_" << trigger;
+    histnamebuilder << "RawJetSpectrum_FullJets_R" <<  std::setw(2) << std::setfill('0') << int(R*10.) << "_" << trigger << "_" << triggercluster;
+    if(sysvar.length()) {
+        dirnamebuilder << "_" << sysvar;
+        histnamebuilder << "_" << sysvar;
+    }
+    reader.cd(dirnamebuilder.str().data());
+    auto histos = static_cast<TKey *>(gDirectory->GetListOfKeys()->At(0))->ReadObject<TList>();
+    auto hspecandclass = static_cast<TH2 *>(histos->FindObject("hJetSpectrum"));
+    auto rawspectrum = hspecandclass->ProjectionY(histnamebuilder.str().data(), clusterbin, clusterbin);
+    rawspectrum->SetDirectory(nullptr);
+
+    // calculate norm
+    auto hnorm = static_cast<TH1 *>(histos->FindObject("hClusterCounter"));
+    auto norm = hnorm->GetBinContent(clusterbin);
+    // calculate bin0 correction
+    auto evhist = static_cast<TH1 *>(histos->FindObject("fNormalisationHist")); // take the one from the AliAnalysisTaskEmcal directly
+    auto bin0correction = evhist->GetBinContent(evhist->GetXaxis()->FindBin("Event selection")) / evhist->GetBinContent(evhist->GetXaxis()->FindBin("Vertex reconstruction and quality"));
+    norm *= bin0correction;
+
+    return {norm, rawspectrum};
+}
+
+TH2 *getResponseMatrix(TFile &reader, double R, const std::string_view sysvar, int closurestatus = 0) {
+    std::string closuretag;
+    switch(closurestatus) {
+    case 0: break;
+    case 1: closuretag = "Closure"; break;
+    case 2: closuretag = "NoClosure"; break;   
+    };
+    std::string responsematrixbase = "hJetResponseFine";
+    std::stringstream responsematrixname;
+    responsematrixname << responsematrixbase;
+    if(closuretag.length()) responsematrixname <<  closuretag;
+    std::stringstream dirnamebuilder;
+    dirnamebuilder << "EnergyScaleResults_FullJet_R" << std::setw(2) << std::setfill('0') << int(R*10.) << "_INT7";
+    if(sysvar.length()) dirnamebuilder << "_" << sysvar;
+    reader.cd(dirnamebuilder.str().data());
+    auto histlist = static_cast<TKey *>(gDirectory->GetListOfKeys()->At(0))->ReadObject<TList>();
+    auto rawresponse = static_cast<TH2 *>(histlist->FindObject(responsematrixname.str().data()));
+    rawresponse->SetDirectory(nullptr);
+    rawresponse->SetNameTitle(Form("Rawresponse_R%02d", int(R*10.)), Form("Raw response R=%.1f", R));
+    return rawresponse;
+}
+
+double getCENTNOTRDCorrection(TFile &reader, const std::string_view sysvar){
+    std::stringstream dirnamebuilder;
+    dirnamebuilder << "JetSpectrum_FullJets_R02_EJ1";
+    if(sysvar.length()) dirnamebuilder << "_" << sysvar; 
+    reader.cd(dirnamebuilder.str().data());
+    auto histos = static_cast<TKey *>(gDirectory->GetListOfKeys()->At(0))->ReadObject<TList>();
+    auto hnorm = static_cast<TH1 *>(histos->FindObject("hClusterCounter"));
+    return hnorm->GetBinContent(3)/hnorm->GetBinContent(2);
+}
+
+TH1 *makeTriggerEfficiency(TFile &mcreader, double R, const std::string_view trigger, const std::string_view sysvar){
+    std::unique_ptr<TH1> mbref(getSpectrumAndNorm(mcreader, R, "INT7", "ANY", sysvar).second),
+                         trgspec(getSpectrumAndNorm(mcreader, R, trigger, "ANY", sysvar).second);
+    auto eff = histcopy(trgspec.get());
+    eff->SetNameTitle(Form("TriggerEfficiency_%s_R%02d", trigger.data(), int(R * 10.)), Form("Trigger efficiency for %s for R=%.1f", trigger.data(), R));
+    eff->SetDirectory(nullptr);
+    eff->Divide(trgspec.get(), mbref.get(), 1., 1., "b");
+    return eff;
+}
+
+TH1 *makeCombinedRawSpectrum(const TH1 &mb, const TH1 &triggered, double ptswap){
+    auto combined = histcopy(&mb);
+    combined->SetDirectory(nullptr);
+    for(auto b : ROOT::TSeqI(combined->GetXaxis()->FindBin(ptswap), combined->GetXaxis()->GetNbins()+1)) {
+        combined->SetBinContent(b, triggered.GetBinContent(b));
+        combined->SetBinError(b, triggered.GetBinError(b));
+    }
+    return combined;
+}
+
+
+void runCorrectionChain1DBayes_SpectrumTaskSimpleFineLow_SysBinVar(const std::string_view datafile, const std::string_view mcfile, const std::string_view binvar, const std::string_view sysvar = ""){
+    ROOT::EnableThreadSafety();
+    int NTHREAD=2;
+    std::stringstream outputfile;
+    outputfile << "correctedBayes_fine_lowpt_binvar" << binvar;
+    if(sysvar.length()) {
+        outputfile << "_" << sysvar;
+    }
+    outputfile << ".root";
+    std::unique_ptr<TFile> datareader(TFile::Open(datafile.data(), "READ")),
+                           mcreader(TFile::Open(mcfile.data(), "READ")),
+                           writer(TFile::Open(outputfile.str().data(), "RECREATE"));
+    auto binningpart = getJetPtBinningNonLinTrueLargeFineLow(),
+         binningdet = getJetPtBinningNonLinSmearLargeFineBV(binvar);
+    auto centnotrdcorrection = getCENTNOTRDCorrection(*datareader, sysvar);
+    double crosssection = 57.8;
+    for(double radius = 0.2; radius <= 0.6; radius += 0.1) {
+        std::cout << "Doing jet radius " << radius << std::endl;
+        auto mbspectrum = getSpectrumAndNorm(*datareader, radius, "INT7", "ANY", sysvar),
+             ej1spectrum = getSpectrumAndNorm(*datareader, radius, "EJ1", "CENTNOTRD", sysvar),
+             ej2spectrum = getSpectrumAndNorm(*datareader, radius, "EJ2", "CENT", sysvar);
+        auto trgeffej1 = makeTriggerEfficiency(*mcreader, radius, "EJ1", sysvar),
+             trgeffej2 = makeTriggerEfficiency(*mcreader, radius, "EJ2", sysvar);
+
+        // apply CENTNOTRD correction
+        ej1spectrum.second->Scale(1./centnotrdcorrection);
+
+        // Correct for the trigger efficiency
+        ej1spectrum.second->Divide(trgeffej1);
+        ej2spectrum.second->Divide(trgeffej2);
+
+        // Rebin all raw level histograms
+        std::unique_ptr<TH1> mbrebinned(mbspectrum.second->Rebin(binningdet.size()-1, "mbrebinned", binningdet.data())),
+                             ej1rebinned(ej1spectrum.second->Rebin(binningdet.size()-1, "ej1rebinned", binningdet.data()));
+        auto hraw = makeCombinedRawSpectrum(*mbrebinned, *ej1rebinned, 80.);
+        hraw->SetNameTitle(Form("hraw_R%02d", int(radius * 10.)), Form("Raw Level spectrum R=%.1f", radius));
+        hraw->Scale(crosssection/mbspectrum.first);
+
+        // Get the response matrix
+        auto rawresponse = getResponseMatrix(*mcreader, radius, sysvar);
+        rawresponse->SetName(Form("%s_fine", rawresponse->GetName()));
+        rawresponse->Scale(40e6);   // undo scaling with the number of trials
+        auto rebinnedresponse  = makeRebinned2D(rawresponse, binningdet, binningpart);
+        rebinnedresponse->SetName(Form("%s_standard", rebinnedresponse->GetName()));
+        std::unique_ptr<TH1> truefulltmp(rawresponse->ProjectionY()),
+                             truetmp(rebinnedresponse->ProjectionY());
+        auto truefull = truefulltmp->Rebin(binningpart.size() - 1, Form("truefull_R%02d", int(radius*10.)), binningpart.data());
+        truefull->SetDirectory(nullptr);
+        auto effkine = truetmp->Rebin(binningpart.size()-1, Form("effKine_R%02d", int(radius*10.)), binningpart.data());
+        effkine->SetDirectory(nullptr);
+        effkine->Divide(effkine, truefull, 1., 1., "b");
+
+        // split the response matrix for the closure test
+        auto responseclosure = getResponseMatrix(*mcreader, radius, sysvar, 1);
+        responseclosure->SetName(Form("%s_fine", responseclosure->GetName()));
+        std::unique_ptr<TH2> truthclosure(getResponseMatrix(*mcreader, radius, sysvar, 2));
+        truthclosure->SetName("truthclosure");
+        responseclosure->Scale(40e6);
+        TH1 *priorsclosuretmp(responseclosure->ProjectionY()),
+            *detclosuretmp(truthclosure->ProjectionX()),
+            *partclosuretmp(truthclosure->ProjectionY());
+        auto priorsclosure = priorsclosuretmp->Rebin(binningpart.size()-1, Form("priorsclosure_R%02d", int(radius*10.)), binningpart.data()),
+             detclosure = detclosuretmp->Rebin(binningdet.size()-1, Form("detclosure_R%02d", int(radius*10.)), binningdet.data()),
+             partclosure = partclosuretmp->Rebin(binningpart.size()-1, Form("partclosure_R%02d", int(radius*10.)), binningpart.data());
+        priorsclosure->SetDirectory(nullptr);
+        detclosure->SetDirectory(nullptr);
+        partclosure->SetDirectory(nullptr);
+        auto rebinnedresponseclosure = makeRebinned2D(responseclosure, binningdet, binningpart);
+        rebinnedresponseclosure->SetName(Form("%s_closure", rebinnedresponseclosure->GetName()));
+
+        RooUnfoldResponse responsematrix(nullptr, truefull, rebinnedresponse),
+                          responsematrixClosure(nullptr, priorsclosure, rebinnedresponseclosure);
+        responsematrix.UseOverflow(false);
+        responsematrixClosure.UseOverflow(false);
+
+        UnfoldingPool work;
+        for(auto ireg : ROOT::TSeqI(2, 10)) {
+            work.InsertWork({ireg, radius, hraw, &responsematrix, detclosure, &responsematrixClosure});
+        }
+
+        std::vector<std::thread> workthreads;
+        std::set<unfoldingResults> unfolding_results;
+        std::mutex combinemutex;
+        for(auto i : ROOT::TSeqI(0, NTHREAD)){
+            workthreads.push_back(std::thread([&combinemutex, &work, &unfolding_results](){
+                UnfoldingRunner worker(&work);
+                worker.DoWork();
+                std::unique_lock<std::mutex> combinelock(combinemutex);
+                for(auto res : worker.getUnfolded()) unfolding_results.insert(res);
+            }));
+        }
+        for(auto &th : workthreads) th.join();
+
+        // Write everything to file
+        writer->mkdir(Form("R%02d", int(radius*10)));
+        writer->cd(Form("R%02d", int(radius*10)));
+        auto basedir = gDirectory;
+        basedir->mkdir("rawlevel");
+        basedir->cd("rawlevel");
+        mbspectrum.second->Write();
+        ej1spectrum.second->Write();
+        ej2spectrum.second->Write();
+        trgeffej1->Write();
+        trgeffej2->Write();
+        hraw->Write();
+        auto hnorm = new TH1F("hNorm", "Norm", 1, 0.5, 1.5);
+        hnorm->SetBinContent(1, mbspectrum.first);
+        hnorm->Write();
+        auto hcntcorr = new TH1F("hCENTNOTRDcorrection", "CENTNOTRD correction", 1, 0.5, 1.5);
+        hcntcorr->SetBinContent(1, centnotrdcorrection);
+        hcntcorr->Write();
+        basedir->mkdir("response");
+        basedir->cd("response");
+        rawresponse->Write();
+        rebinnedresponse->Write();
+        truefull->Write();
+        effkine->Write();
+        basedir->mkdir("closuretest");
+        basedir->cd("closuretest");
+        priorsclosure->Write("priorsclosure");
+        detclosure->Write("detclosure");
+        partclosure->Write("partclosure");
+        responseclosure->Write("responseClosureFine");
+        rebinnedresponseclosure->Write("responseClosureRebinned");
+        for(auto reg : unfolding_results){
+            basedir->mkdir(Form("reg%d", reg.fReg));
+            basedir->cd(Form("reg%d", reg.fReg));
+            if(reg.fUnfolded) reg.fUnfolded->Write();
+            if(reg.fNormalized) reg.fNormalized->Write();
+            if(reg.fBackfolded) reg.fBackfolded->Write();
+            if(reg.fDvector) reg.fDvector->Write();
+            if(reg.fPearson) reg.fPearson->Write();
+            if(reg.fUnfoldedClosure) reg.fUnfoldedClosure->Write();
+            if(reg.fDvectorClosure) reg.fDvectorClosure->Write();
+            if(reg.fPearsonClosure) reg.fPearsonClosure->Write();
+        }
+    }
+}
