259c9babe56da11bc0b58bf9d5ee7db49a4fa22d
Markus Fasel <markus.fasel@cern.ch>
2019-05-08 16:04:26 +0200

    Adding plotters for relative statistical uncertainty

---
+++ unfolding/normalize1D/plotStatRelRaw.cpp
@@ -0,0 +1,36 @@
+#include "../../meta/stl.C"
+#include "../../meta/root.C"
+#include "../../meta/root6tools.C"
+#include "../../struct/JetSpectrumReader.cxx"
+#include "../../struct/GraphicsPad.cxx"
+
+void plotStatRelRaw(const std::string_view inputfile){
+    std::vector<std::string> spectra = {"hraw"};
+    JetSpectrumReader reader(inputfile, spectra);
+    
+    auto plot = new ROOT6tools::TSavableCanvas("relStatErrorRaw", "Relative statistical error of the raw spectrum", 1200, 800);
+    plot->Divide(3,2);
+
+    Style statstyle{kBlue, 20};
+
+    int ipad(1);
+    for(auto r : reader.GetJetSpectra().GetJetRadii()){
+        plot->cd(ipad++);
+        std::string rstring(Form("R%02d", int(r*10.)));
+        GraphicsPad errorpad(gPad);
+        errorpad.Frame(Form("errorpad%s", rstring.data()), "p_{t} (GeV/c)", "rel. stat. error", 0., 240., 0., 0.1);
+        errorpad.Margins(0.12, 0.05, -1, 0.05);
+        errorpad.Label(0.2, 0.86, 0.4, 0.93, Form("R=%.1f", r));
+        auto spec = reader.GetJetSpectrum(r, spectra[0]);
+        auto relstat = (TH1 *)spec->Clone(Form("relStat%s",rstring.data()));
+        for(auto b : ROOT::TSeqI(0, relstat->GetXaxis()->GetNbins())){
+            relstat->SetBinContent(b+1, relstat->GetBinError(b+1)/relstat->GetBinContent(b+1));
+            relstat->SetBinError(b+1, 0.);
+        }
+        relstat->SetLineColor(kBlue);
+        relstat->Draw("same");
+    }
+    plot->cd();
+    plot->Update();
+    plot->SaveCanvas(plot->GetName());
+}
