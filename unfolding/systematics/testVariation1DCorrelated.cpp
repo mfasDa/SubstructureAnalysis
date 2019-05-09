d89c9eab2a2a92760f2ed58af2d3b0f784629400
Markus Fasel <markus.fasel@cern.ch>
2019-05-09 20:18:02 +0200

    Adapt variation tester to new structure

--- unfolding/systematics/testVariation1DCorrelated.cpp
+++ unfolding/systematics/testVariation1DCorrelated.cpp
@@ -16,7 +16,7 @@ struct outputdata{
     TH1*                fDefaultSpectrum;
     TH1*                fVarSpectrum;
     TH1*                fBarlow;
-    TH1 *               fAbsDiff;
+    TH1*                fAbsDiff;
     Ratio*              fRatio;
 
     bool operator<(const outputdata &other) const { return fR < other.fR; }
@@ -27,11 +27,12 @@ std::map<int, TH1 *> getCorrected(const std::string_view filename, const std::st
     std::vector<std::string> spectra = {Form("normalized_reg%d", regularization)};
     std::map<int, TH1 *> result;
     JetSpectrumReader reader(filename, spectra);
-    for(auto radius : reader.GetJetSpectra().GetJetRadii()) {
-        auto spec = reader.GetJetSpectrum(radius, spectra[0]);  
-        spec->SetNameTitle(Form("Spec%s_R%02d", varname.data(), int(radius*10.)), varname.data());
+    for(auto r : reader.GetJetSpectra().GetJetRadii()) {
+        std::cout << "Read " << r << std::endl;
+        auto spec = reader.GetJetSpectrum(r, spectra[0]);  
+        spec->SetNameTitle(Form("Spec%s_R%02d", varname.data(), int(r*10.)), varname.data());
         spec->SetDirectory(nullptr);
-        result[int(radius*10)] = spec;
+        result[int(r*10.)] = spec;
     }
     return result;
 }
@@ -48,46 +49,53 @@ TH1 *makeBarlow(const TH1 *variation, const TH1 *defaultdist){
     return barlow;
 }
 
-void testVariation1D(const std::string_view varname, const std::string_view vartitle, const std::string_view defaultfile, const std::string_view varfile, int regularizationDefault = 4, int regularizationVar = 4){
-    auto specDefault = getCorrected(defaultfile, "default", regularizationDefault), specVariation = getCorrected(varfile, varname, regularizationVar);
+void testVariation1DCorrelated(const std::string_view varname, 
+                               const std::string_view vartitle, 
+                               const std::string_view filesuperset, 
+                               const std::string_view filesubset, 
+                               bool defaultIsSubset, 
+                               int regularization = 4){
+    std::string varnamesubset = (defaultIsSubset ? "default" : varname.data()),
+                varnamesuperset = (defaultIsSubset ? varname.data() : "default");
+    auto specSuperset = getCorrected(filesuperset, varnamesuperset , regularization), specSubset = getCorrected(filesubset, varnamesubset, regularization);
     Style defaultstyle{kRed, 24}, varstyle{kBlue, 25};
-    gROOT->cd();
 
     std::set<outputdata> output;
     double ptmax = 350.;
-    for(double radius = 0.2; radius <= 0.6; radius += 0.1){
-        //if(TMath::Abs(radius-0.3) < 1e-5) continue;
-        printf("Doing r=%.1f\n", radius);
-        std::string rstring(Form("R%02d", int(radius*10.)));
-        auto compplot = new ROOT6tools::TSavableCanvas(Form("comp_%s_%s", varname.data(), rstring.data()), Form("Comparison %s, R = %.1f", varname.data(), radius), 1200, 1000);
+    for(double r = 0.2; r <= 0.6; r+= 0.1){
+        printf("Doing r=%.1f\n", r);
+        std::string rstring(Form("R%02d", int(r*10.)));
+        auto compplot = new ROOT6tools::TSavableCanvas(Form("comp_%s_%s", varname.data(), rstring.data()), Form("Comparison %s, R = %.1f", varname.data(), r), 1200, 1000);
         compplot->Divide(3,1);
 
         compplot->cd(1);
         GraphicsPad specpad(gPad);
         specpad.Logy();
         specpad.Frame(Form("compframe_%s", rstring.data()), "p_{t} (GeV/c)", "dsigma/(dp_{t} d#eta) (mb/(GeV/c))", 0., ptmax, 1e-10, 1e-1);
-        specpad.Label(0.15, 0.15, 0.45, 0.21, Form("jets, R=%.1f", radius));
-        specpad.Legend(0.55, 0.7, 0.89, 0.89);
+        specpad.Label(0.15, 0.15, 0.45, 0.21, Form("jets, R=%.1f", r));
+        specpad.Legend(0.35, 0.7, 0.89, 0.89);
 
-        auto foundDefault = specDefault.find(int(radius*10.)),
-             foundVariation = specVariation.find(int(radius*10.)); 
-        if(foundDefault == specDefault.end()) std::cout << "default " << radius << " not found" << std::endl;
-        if(foundVariation == specVariation.end()) std::cout << "variation " << radius << " not found" << std::endl;
-        auto defaultspec = foundDefault->second,
-             varspec = foundVariation->second;
+        auto foundSuper = specSuperset.find(int(r*10.)),
+             foundSub = specSubset.find(int(r*10.)); 
+        if(foundSuper == specSuperset.end()) std::cout << "super " << r << " not found" << std::endl;
+        if(foundSub == specSubset.end()) std::cout << "sub " << r << " not found" << std::endl;
+        auto superspec = foundSuper->second,
+             subspec = foundSub->second,
+             defaultspec = defaultIsSubset ? subspec : superspec,
+             varspec = defaultIsSubset ? superspec : subspec;
         specpad.Draw<TH1>(defaultspec, defaultstyle, "Default");
         specpad.Draw<TH1>(varspec, varstyle, vartitle);
 
         compplot->cd(2);
         GraphicsPad ratiopad(gPad);
         ratiopad.Frame(Form("ratioframe_%s", rstring.data()), "p_{t} (GeV/c)", "variation / default", 0., ptmax, 0.5, 1.5);
-        Ratio *varratio = new Ratio(varspec, defaultspec);
+        Ratio *varratio = new Ratio(superspec, subspec, "b");
         ratiopad.Draw<Ratio>(varratio, defaultstyle);
 
         compplot->cd(3);
         GraphicsPad barlowpad(gPad);
         barlowpad.Frame("barlowframe_%d", "p_{t} (GeV/c)", "(default-variation)/(#sqrt{|#sigma_{var}^{2} - sigma_{default}^{2}|})", 0., ptmax, 0., 10.);
-        auto barlow = makeBarlow(varspec, defaultspec);
+        auto barlow = makeBarlow(superspec, subspec);
         barlow->SetNameTitle("barlowtest", "Barlow test");
         barlow->SetLineColor(kBlack);
         barlow->SetFillColor(kRed);
@@ -105,7 +113,7 @@ void testVariation1D(const std::string_view varname, const std::string_view vart
             absdiff->SetBinError(b+1, 0.);
         }
 
-        output.insert({radius, defaultspec, varspec, barlow, absdiff, varratio}); 
+        output.insert({r, defaultspec, varspec, barlow, absdiff, varratio}); 
     }
 
     // Create output rootfile
