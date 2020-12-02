9a28cfb2b6c31d0e6533509f7d43843a2019a8fb
mfasel <markus.fasel@cern.ch>
2020-12-02 14:19:18 +0100

    add simple cleanup script for mcgen repo dirs

---
+++ MCgen/cleanupInputDir.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+BASE=$PWD
+INPUTDIR=$1
+cd $INPUTDIR
+tookeep=(env.sh MLTrainDefinition.cfg globalvariables.C generator_customization.C handlers.C)
+for f in `ls -1`; do
+    found=0
+    for t in ${tookeep[@]}; do
+        if [ $f == $t ]; then found=1; break; done
+        if [ $found -eq 0 ]; then rm $f; fi
+    done
+done
+cd $BASE
