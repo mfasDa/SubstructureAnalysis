913cc9ebb302534f470b51683801f0bf82462908
mfasel <markus.fasel@cern.ch>
2021-05-10 11:33:04 +0200

    Add separate submitter script (i.e. for period-wise merging)

---
+++ merge/submitMergeFinal.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import argparse
+import os
+import subprocess
+import sys
+
+if __name__ == "__main__":
+    repo = os.path.dirname(os.path.abspath(sys.argv[0]))
+    executable = os.path.join(repo, "processMergeFinal.sh")
+
+    parser = argparse.ArgumentParser("submitMergeFinal.py", description="Submitter for final merge job")
+    parser.add_argument("inputdir", metavar="INPUTDIR", type=str, help="Iput directory for merge")
+    parser.add_argument("-r", "--rootfile", metavar="ROOTFILE", type=str, default="AnalysisResults.root", help="Name of the ROOT file")
+    parser.add_argument("-p", "--partition", metavar="PARTITION", type=str, default="vip", help="Queue for running")
+    args = parser.parse_args()
+
+    cmd = "{EXE} {INPUTDIR} {ROOTFILE}".format(EXE=executable, INPUTDIR=args.inputdir, ROOTFILE=args.rootfile)
+    logfile = os.path.join(args.inputdir, "merge_final.log")
+    jobname = "merge_final"
+    submitcmd = "sbatch -N1 -n1 --partition={PARTITION} -J {JOBNAME} -o {LOGFILE} {PROGRAM}".format(PARTITION=args.partition, JOBNAME=jobname, LOGFILE=logfile, PROGRAM=cmd)
+    subprocess.call(submitcmd, shell=True)
