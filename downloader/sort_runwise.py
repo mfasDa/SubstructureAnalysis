855fa9e737d51f71ca4463d4c6c7f66cb4614d4d
mfasel <markus.fasel@cern.ch>
2021-05-10 11:33:41 +0200

    Add sorter scripts for runwise and period-wise sorting of pt-hard productions
    Period-wise sorter making use of internal runlists.

--- downloader/sort_runwise.py
+++ downloader/sort_runwise.py
@@ -6,53 +6,14 @@ import os
 import shutil
 import sys
 
-class SampleDB:
+class RunSorter:
 
-    def __init__(self, basedir):
-        self.__basedir = os.path.join(basedir, "runlists_EMCAL")
-        self.__runlists = {}
-        self.__initialize()
-
-    def find_period(self, run: int) -> str:
-        period = ""
-        for testperiod, runs in self.__runlists.items():
-            if run in runs:
-                period = testperiod
-                break
-        return period
-
-    def __initialize(self):
-        nruns = 0
-        skip_periods = ["LHC18g", "LHC18h", "LHC18i", "LHC18j", "LHC18k"]
-        for period in os.listdir(self.__basedir):
-            if period in skip_periods:
-                # Skip LHC18g-k in favor of the combined period
-                continue
-            logging.debug("Adding runs for period %s", period)
-            self.__runlists[period] = self.__read_runlist(os.path.join(self.__basedir, period))
-            nruns += len(self.__runlists[period])
-        logging.info("Sample DB initialized with %d runs", nruns)
-
-    def __read_runlist(self, filename: str) -> list:
-        runs = []
-        with open(filename, "r") as runreader:
-            for line in runreader:
-                for en in line.split(","):
-                    runstring = en.lstrip().rstrip()
-                    if not len(runstring):
-                        continue
-                    runs.append(int(runstring))
-        return runs
-
-class SampleSorter:
-
-    def __init__(self, inputdir: str, outputdir: str, rootfile: str, sampledb: SampleDB):
+    def __init__(self, inputdir: str, outputdir: str, rootfile: str):
         self.__inputdir = inputdir
         self.__outputdir = outputdir
         self.__rootfile = rootfile
         self.__tag = self.__read_tag(os.path.basename(self.__inputdir))
         print("determined tag {}".format(self.__tag))
-        self.__sampleDB = sampledb
 
     def __read_tag(self, production: str) -> str:
         print("Creating tag from {}".format(production))
@@ -62,32 +23,28 @@ class SampleSorter:
             tag = "{}_{}".format(tokens[len(tokens)-2], tokens[len(tokens)-1])
         return tag
 
-    def has_period(self, period: str) -> str:
-        teststring = "{}_{}".format(period, self.__tag)
+    def has_rundir(self, run: int) -> str:
+        teststring = "{}_{}".format(run, self.__tag)
         for testperiod in os.listdir(self.__outputdir):
             if testperiod ==  teststring:
                 return testperiod
         return ""
 
-    def init_period(self, period: str) -> str:
-        logging.info("Initializing directory structure for period %s", period)
-        perioddir = os.path.join(self.__outputdir, "{}_{}".format(period, self.__tag))
-        os.makedirs(perioddir, 0o755)
+    def init_run(self, run: str) -> str:
+        logging.info("Initializing directory structure for period %s", run)
+        rundir = os.path.join(self.__outputdir, "{}_{}".format(run, self.__tag))
+        os.makedirs(rundir, 0o755)
         for ipth in range(1, 21):
-            pthardbindir = os.path.join(perioddir, "%02d" %ipth)
+            pthardbindir = os.path.join(rundir, "%02d" %ipth)
             os.makedirs(pthardbindir, 0o755)
-        return os.path.basename(perioddir)
+        return os.path.basename(rundir)
 
     def copy_to_target(self, inputfile: str, run: int, pthardbin: int):
-        period = self.__sampleDB.find_period(run)
-        if not len(period):
-            logging.error("No period found for run %d", run)
-            return
-        targetdir = self.has_period(period)
-        logging.debug("Inputfile: %s, run: %s, pthardin %s, period %s, targetdir %s", inputfile, run, pthardbin, period, targetdir)
+        targetdir = self.has_rundir(run)
+        logging.debug("Inputfile: %s, run: %s, pthardin %s, targetdir %s", inputfile, run, pthardbin, targetdir)
         if not len(targetdir):
-            targetdir = self.init_period(period)
-        outputdir = os.path.join(self.__outputdir, targetdir, "%02d" %pthardbin, "%d" %run)
+            targetdir = self.init_run(run)
+        outputdir = os.path.join(self.__outputdir, targetdir, "%02d" %pthardbin)
         if not os.path.exists(outputdir):
             os.makedirs(outputdir, 0o755)
         outputfile = os.path.join(outputdir, os.path.basename(inputfile))
@@ -117,11 +74,10 @@ if __name__ == "__main__":
         loglevel = logging.DEBUG
     logging.basicConfig(format="[%(levelname)s]: %(message)s", level=loglevel)
 
-    sampeldb = SampleDB(repo)
     subsamples = [x for x in os.listdir(args.inputdir) if "LHC" in x]
     for subsample in subsamples:
         logging.info("Sorting subsample %s", subsample)
         if not os.path.exists(args.outputdir):
             os.makedirs(args.outputdir, 0o755)
-        sorter = SampleSorter(os.path.join(args.inputdir, subsample), args.outputdir, args.rootfile, sampeldb)
+        sorter = RunSorter(os.path.join(args.inputdir, subsample), args.outputdir, args.rootfile)
         sorter.sort()
