50c3f7c9d601e24613eff59a15f38084c57fabf1
mfasel <markus.fasel@cern.ch>
2021-08-18 15:36:00 +0200

    FIx sum and add alienv command

--- SubstructureHelpers/slurm.py
+++ SubstructureHelpers/slurm.py
@@ -13,12 +13,14 @@ class ScriptWriter:
             return "Working directory needs to be defined"
 
     def __init__(self, filename):
+        self.__name = filename
         self.__fileio = open(filename, "w")
         self.__writeline("#! /bin/bash")
         self.__workdirSet = False
 
     def __del__(self):
-        self.__fileio.close()
+        if not self.__fileio.closed:
+            self.__fileio.close()
 
     def __writeline(self, line: str):
         self.__fileio.write("{}\n".format(line))
@@ -39,7 +41,11 @@ class ScriptWriter:
         self.sbatch("-J {}".format(jobname))
 
     def logfile(self, logfile: str):
-        self.sbatch("-o {}".format(logfile))
+        abslogfile = os.path.abspath(logfile)
+        logdir = os.path.dirname(abslogfile)
+        if not os.path.exists(logdir):
+            os.makedirs(logdir, 0o755)
+        self.sbatch("-o {}".format(abslogfile))
 
     def partition(self, partition: str):
         self.sbatch("--partition={}".format(partition))
@@ -52,6 +58,9 @@ class ScriptWriter:
 
     def Array(self, minid: int, maxid: int):
         self.sbatch("--array={}-{}".format(minid, maxid))
+
+    def dependency(self, dependency: int):
+        self.sbatch("--dependency={}".format(dependency))
 
     def workdir(self, directory):
         self.__writeline("export WORKDIR={}".format(directory))
@@ -73,11 +82,11 @@ class ScriptWriter:
         self.__writeline("{}={}".format(variable, value))
 
     def sum(self, variable: str, values: list):
-        cmd="{}=$(("
+        cmd="{}=$((".format(variable)
         first = True
         for value in values:
             if not first:
-                cmd += "+ "
+                cmd += "+"
             else:
                 first = False
             cmd += "{}".format(value)
@@ -87,6 +96,15 @@ class ScriptWriter:
     def modules(self, modules: list):
         for mod in modules:
             self.__writeline("module load {}".format(mod))
+
+    def alienv(self, packages: list):
+        self.define("ALIENV", "`which alienv`")
+        instruction = "eval `$ALIENV --no-refresh load"
+        for package in packages:
+            instruction += " {}".format(package)
+        instruction += "`"
+        self.instruction(instruction)
+        self.instruction("eval `$ALIENV list`")
 
     def process(self, executable: str, args: list = [], logfile: str = ""):
         cmd = executable
@@ -113,6 +131,11 @@ class ScriptWriter:
             raise ScriptWriter.WorkdirNotSetException
         self.cd("/")
         self.rmdir("$WORKDIR")
+
+    def submit(self) -> int:
+        if not self.__fileio.closed:
+            self.__fileio.close()
+        return submit_script(self.__name)
         
 
 def submit_script(jobscript: str):
