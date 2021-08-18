8212499800c41b00d11d5c7297c24a799d65007e
mfasel <markus.fasel@cern.ch>
2021-08-18 15:12:24 +0200

    Add option to add period to table

--- helpers/csvreader.C
+++ helpers/csvreader.C
@@ -3,13 +3,16 @@ class Table {
 public:
 
 	struct Row {
-		std::map<std::string, int> entry;
+		std::map<std::string, std::string> entry;
+
+		Row() = default;
+		~Row() = default;
 	};
 
 	Table() = default;
 	~Table() = default;
 
-	Row *getRow(std::string key, int value) {
+	Row *getRow(std::string key, std::string value) {
 		Row *result = nullptr;
 		auto found = std::find_if(mRows.begin(), mRows.end(), [key, value](const Row &row){
 			auto test = row.entry.find(key);
@@ -21,13 +24,12 @@ public:
 		return result;
 	};
 
-	void insertRow(const std::map<std::string, int> &row) {
-		Row nextrow;
-		nextrow.entry = row;
-		mRows.emplace_back(row);
+	void insertRow(const std::map<std::string, std::string> &row) {
+		mRows.push_back({row});
 	}
 
-	const std::vector<Row> getRows() const { return mRows; }
+	std::vector<Row> &getRows() { return mRows; }
+	const std::vector<Row> &getRows() const { return mRows; }
 
 	void importRows(const Table &other){
 		for(const auto &row : other.getRows()) { insertRow(row.entry); }		
@@ -44,17 +46,19 @@ Table decodeCSV(const std::string filename) {
 	bool first = true;
 	Table result;
 	while(std::getline(in, tmp)) {
-		std::stringstream decoder;
+		std::stringstream decoder(tmp);
 		std::string key;
 		if(first) {
 			// Decode column names
-			while(std::getline(decoder, key, ',')) colnames.emplace_back(key);
+			while(std::getline(decoder, key, ',')) {
+				colnames.emplace_back(key);
+			}
 			first = false;
 		} else {
-			std::map<std::string, int> row;
+			std::map<std::string, std::string> row;
 			int current = 0;
 			while(std::getline(decoder, key, ',')) {
-				row[colnames[current]] = std::stoi(key);
+				row[colnames[current]] = key;
 				current++;
 			}
 			result.insertRow(row);
--- helpers/triggerRawDB.C
+++ helpers/triggerRawDB.C
@@ -7,18 +7,29 @@ public:
 	TriggerRawDB() = default;
 	~TriggerRawDB() = default;
 
-	bool hasRun(int runnumber) { return mData.getRow("run", runnumber) != nullptr; }
+	bool hasRun(int runnumber) { return mData.getRow("run", Form("%d", runnumber)) != nullptr; }
 	int getTriggersForRun(int runnumber, const char *trigger) {
-		auto tmp = mData.getRow("run", runnumber);
+		auto tmp = mData.getRow("run", Form("%d", runnumber));
 		if(tmp) {
 			auto found = tmp->entry.find(trigger);
-			if(found != tmp->entry.end()) return found->second;
+			if(found != tmp->entry.end()) return std::stoi(found->second);
 		}
 		return 0;
 	}
+	std::string getPeriod(int runnumber){
+		auto tmp = mData.getRow("run", Form("%d", runnumber));
+		if(tmp) {
+			auto found = tmp->entry.find("period");
+			if(found != tmp->entry.end()) return found->second;
+		}
+		return "";
+	}
 
-	void addFile(std::string filename){
+	void addFile(std::string filename, std::string period){
 		auto tmp = decodeCSV(filename);
+		for(auto &row : tmp.getRows()) {
+			row.entry[std::string("period")] = period;
+		}
 		if(mData.getRows().size()) mData.importRows(tmp);
 		else mData=tmp;
 	}
@@ -27,7 +38,7 @@ public:
 		int yeartag = year % 100;
 		std::string csvdir = Form("%s/RawEventCounts", gSystem->Getenv("SUBSTRUCTURE_ROOT"));
 		std::vector<std::string> filenames;
-		auto filestring = gSystem->GetFromPipe(Form("ls -1 %s | grep LHC%s | grep -v bad"));
+		auto filestring = gSystem->GetFromPipe(Form("ls -1 %s | grep LHC%d | grep -v bad", csvdir.data(), yeartag));
 		std::unique_ptr<TObjArray> tokened(filestring.Tokenize("\n"));
 		for(auto f : TRangeDynCast<TObjString>(tokened.get())) {
 			filenames.emplace_back(f->String().Data());
@@ -35,7 +46,9 @@ public:
 		std::sort(filenames.begin(), filenames.end(), std::less<std::string>());
 		for(auto f : filenames) {
 			std::string path = Form("%s/%s", csvdir.data(), f.data());
-			addFile(path);
+			std::string period = f.substr(0, f.find(".csv"));
+			std::cout << "Reading " << path << std::endl;
+			addFile(path, period);
 		}
 	}
 
