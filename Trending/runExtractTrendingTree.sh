3a57341230e0fb07507a455486e415192109da0a
mfasel <markus.fasel@cern.ch>
2022-01-12 18:37:51 +0100

    Fix path to trending extractor

--- Trending/runExtractTrendingTree.sh
+++ Trending/runExtractTrendingTree.sh
@@ -4,10 +4,11 @@ SOURCEDIR=$1
 WORKDIR=$2
 RUNNUMBER=$3
 
-TRENDINGMACRO=$SOURCEDIR/trendingExtractorData.cd
+TRENDINGMACRO=$SOURCEDIR/Trending/trendingExtractorData.C
 ALIENV=`which alienv`
 export ALIBUILD_WORK_DIR=/software/mfasel/alice/sw
 eval `$ALIENV --no-refresh printenv AliPhysics/latest`
 
 cd $WORKDIR
 cmd=$(printf "root -l -b -q \'%s(\"AnalysisResults.root\", %d)\'" $TRENDINGMACRO $RUNNUMBER)
+eval $cmd
