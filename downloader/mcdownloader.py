0e5543653d88e1e9713d69283d856ee48c08066c
mfasel <markus.fasel@cern.ch>
2022-02-22 15:00:55 +0100

    Add single sample downloader
    - Extract sample downloader to
      separate class
    - Add script for single sample
      downloader
    - Add use sample downloader in
      multi-sample downloader

---
+++ downloader/mcdownloader.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python3
+
+import logging
+import os
+
+from SubstructureHelpers.train import AliTrainDB
+from SubstructureHelpers.cluster import is_valid_partition, PartitionException
+from SubstructureHelpers.slurm import submit
+from merge.submitMergeRun import merge_submitter_runs
+
+class MCDownloadHandler:
+
+    def __init__(self, cluster: str, outputbase: str , trainrun: int, legotrain: str, mergefile: str = "AnalysisResults.root", check: bool = False):
+        self._repo = os.getenv("SUBSTRUCTURE_ROOT")
+        self._outputbase = outputbase
+        self._legotrain = legotrain
+        self._trainrun = None
+        self._cluster = cluster
+        self._partitionDownload = "long"
+        self._mergefile = mergefile
+        self._check = check
+        self._tokens = {"cert": None, "key": None}
+        self._maxtime = "10:00:00"
+
+        pwg,trainname = self._legotrain.split("/")
+        trainDB = AliTrainDB(pwg, trainname)
+        try:
+            self._trainrun = trainDB.getTrainIdentifier(trainrun)
+        except AliTrainDB.UninitializedException as e:
+            logging.error("%s", e)
+        except AliTrainDB.TrainNotFoundException as e:
+            logging.error("%s", e)
+
+    def set_partition_for_download(self, partition: str):
+        if not is_valid_partition(self._cluster, partition):
+            raise PartitionException(partition, self._cluster)
+        self._partitionDownload = partition
+
+    def set_maxtime(self, maxtime: str):
+        self._maxtime = maxtime
+
+    def set_token(self, cert: str, key: str):
+        self._tokens["cert"] = cert
+        self._tokens["key"] = key
+
+    def submit_sample(self, sample: str):
+        if not self._trainrun:
+            logging.error("Failed initializing train run")
+            return
+        jobid_download = self.submit_download(sample)
+        if not jobid_download:
+            return
+        logging.info("Submitting download job with ID: %d", jobid_download)
+        jobid_merge =self.submit_merge(sample, jobid_download, "2:00:00")
+        logging.info("Submitting merge job with ID: %d", jobid_merge)
+        return {"jobid_download": jobid_download, "jobid_merge": jobid_merge}
+
+    def submit_download(self, sample: str) -> int:
+        cert = self._tokens["cert"]
+        key = self._tokens["key"]
+        if not key or not cert:
+            logging.error("Alien token not provided - cannot download ...")
+            return None
+        executable = os.path.join(self._repo, "downloader", "runDownloadAndMergeMCBatch.sh")
+        jobname = "down_{SAMPLE}".format(SAMPLE=sample)
+        outputdir = os.path.join(self._outputbase, sample)
+        if not os.path.exists(outputdir):
+            os.makedirs(outputdir, 0o755)
+        logfile = os.path.join(outputdir, "download.log")
+        
+        downloadcmd = "{EXE} {DOWNLOADREPO} {OUTPUTDIR} {DATASET} {LEGOTRAIN}/{TRAINID} {ALIEN_CERT} {ALIEN_KEY}".format(EXE=executable, DOWNLOADREPO=self._repo, OUTPUTDIR=outputdir, DATASET=sample, LEGOTRAIN=self._legotrain, TRAINID=self._trainrun, ALIEN_CERT=cert, ALIEN_KEY=key)
+        jobid = submit(command=downloadcmd, jobname=jobname, logfile=logfile, partition=self._partitionDownload, numnodes=1, numtasks=4, maxtime=self._maxtime)
+        return jobid
+
+    def submit_merge(self, sample: str, wait_jobid: int, maxtime: str) -> int:
+        workdir = os.path.join(self._outputbase, sample)
+        jobids = merge_submitter_runs(os.path.join(self._repo, "merge"), workdir, self._mergefile, "short", maxtime, wait_jobid, self._check)
+        return jobids["final"]
+
--- downloader/submitDownloadSample.py
+++ downloader/submitDownloadSample.py
@@ -8,50 +8,16 @@ import sys
 from SubstructureHelpers.setup_logging import setup_logging
 from SubstructureHelpers.alien import test_alien_token, recreate_token
 from SubstructureHelpers.cluster import get_cluster, SubmissionHostNotSupportedException, PartitionException, UnknownClusterException
-from merge.submitMergeMCDatasets import merge_submitter_datasets
 from downloader.mcdownloader import MCDownloadHandler
-
-class MultiSampleDownloadHandler(MCDownloadHandler):
-
-    def submit(self, year: int, subsample: str = ""):
-        if not self._trainrun:
-            logging.error("Failed initializing train run")
-            return
-        mcsamples = {2016: ["LHC19a1a_1", "LHC19a1a_2", "LHC19a1b_1", "LHC19a1b_2", "LHC19a1c_1", "LHC19a1c_2"], 2017: ["LHC18f5_1", "LHC18f5_2"], 2018: ["LHC19d3_1", "LHC19d3_1_extra", "LHC19d3_2", "LHC19d3_2_extra"]}
-        if not year in mcsamples.keys():
-            logging.error("No sample or year %d", year)
-        if len(subsample):
-            if not subsample in mcsamples[year]:
-                logging.error("Requested subsample %s not found for year %d ...", subsample, year)
-                return
-        jobids_merge = []
-        for sample in mcsamples[year]:
-            select = False
-            if len(subsample):
-                if sample == subsample:
-                    select = True
-            else:
-                select = True
-            if not select:
-                continue
-            jobids_sample = self.submit_sample(sample) 
-            jobids_merge.append(jobids_sample["jobid_merge"])
-        if len(jobids_merge) > 1:
-            # if we have at least 2 subsamples submit also period merging
-            self.submit_merge_samples(jobids_merge, "2:00:00")
-
-    def submit_merge_samples(self, wait_jobids: list, maxtime: str) -> int:
-        merge_submitter_datasets(os.path.join(self._repo, "merge"), self._outputbase, self._mergefile, "short", maxtime, wait_jobids, self._check)
 
 if __name__ == "__main__":
     currentbase = os.getcwd()
     parser = argparse.ArgumentParser("submitDownloadAndMergeMC.py", description="submitter for download and merge")
     parser.add_argument("-o", "--outputdir", metavar="VARIATION", type=str, default=currentbase, help="Output directory (default: current directory)")
-    parser.add_argument("-y", "--year", metavar="YEAR", type=int,required=True, help="Year of the sample")
+    parser.add_argument("-s", "--sample", metavar="SAMPLE", type=str, default="", help="Copy only subsample")
     parser.add_argument("-t", "--trainrun", metavar="TRAINRUN", type=int, required=True, help="Train run (only main number)")
     parser.add_argument("-f", "--filename", metavar="FILENAME", type=str, default="AnalysisResults.root", help="File to be merged (default: AnalysisResults.root")
     parser.add_argument("-l", "--legotrain", metavar="LEGOTRAIN", type=str, default="PWGJE/Jets_EMC_pp_MC", help="Name of the lego train (default: PWGJE/Jets_EMC_pp_MC)")
-    parser.add_argument("-s", "--subsample", metavar="SUBSAMPLE", type=str, default="", help="Copy only subsample")
     parser.add_argument("-p", "--partition", metavar="PARTITION", type=str, default="long", help="Partition for download")
     parser.add_argument("--maxtime", metavar="MAXTIME", type=str, default="01:00:00", help="Maximum time for download job")
     parser.add_argument("-c", "--check", action="store_true", help="Run check of pt-hard distribution")
@@ -77,12 +43,12 @@ if __name__ == "__main__":
     cert = tokens["cert"]
     key = tokens["key"]
 
-    handler = MultiSampleDownloadHandler(cluster=cluster, outputbase=args.outputdir, trainrun=args.trainrun, legotrain=args.legotrain, check=args.check)
+    handler = MCDownloadHandler(cluster=cluster, outputbase=os.path.abspath(args.outputdir), trainrun=args.trainrun, legotrain=args.legotrain, check=args.check)
     handler.set_token(cert, key)
     handler.set_maxtime(args.maxtime)
     try:
         handler.set_partition_for_download(args.partition)
-        handler.submit(args.year)
+        handler.submit_sample(args.sample)
     except UnknownClusterException as e:
         logging.error("Submission error: %s", e)
         sys.exit(1)
